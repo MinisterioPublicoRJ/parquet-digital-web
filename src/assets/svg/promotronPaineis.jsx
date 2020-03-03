cba0e48541a9684835be61d984ffb3fc60e7d186
Layla <laylacsa@msn.com>
2020-03-03 11:15:34 -0300

    Linting svg and overwriting h2 default padding on the sectiontitle component

--- src/assets/svg/promotronPaineis.jsx
+++ src/assets/svg/promotronPaineis.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// TODO: add proptypes
 
 export default function({ height, width }) {
   return (
@@ -491,5 +492,5 @@ export default function({ height, width }) {
         </g>
       </g>
     </svg>
-  )
+  );
 }

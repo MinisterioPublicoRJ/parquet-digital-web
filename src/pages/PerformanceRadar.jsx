8d13b7abb3ce9513c35c7189f3ddd231b639fc8f
Layla <laylacsa@msn.com>
2020-03-04 15:55:36 -0300

    Right side of the screen mocked

--- src/pages/PerformanceRadar.jsx
+++ src/pages/PerformanceRadar.jsx
@@ -1,5 +1,48 @@
 import React from 'react';
 
-export default function() {
-  return <div> I AM A PAGE, YAY!</div>;
+import { SectionTitle } from '../components';
+import './performanceRadarStyles.css';
+
+class PerformanceRadar extends React.Component {
+  constructor(props) {
+    super(props);
+    const percentagePhrase = ' 10% abaixo ';
+    const movements = 58;
+    this.state = { percentagePhrase, movements};
+    this.getPerformanceData();
+  }
+
+  getPerformanceData() {
+    console.log('I HELPS!');
+  }
+
+  render() {
+    const { dashboard } = this.props;
+    const { percentagePhrase, movements } = this.state;
+
+    if (!percentagePhrase || !movements) return <div>Carregando</div>;
+
+    return (
+      <article className="page radar">
+        <div className="radarLeft">graph goes here</div>
+        <div className="radarRight">
+          {/* <SectionTitle value="resumo do dia" /> */}
+          <p className="paragraphWrapper">
+            Analisamos a atuação da sua promotoria e percebemos que a quantidade de arquivamento
+            está
+            <span style={{ fontWeight: 'bold' }}>{percentagePhrase}</span>
+            da média da casa.
+          </p>
+          <p className="paragraphWrapper">
+            <span style={{ fontWeight: 'bold' }}>Parabéns </span>
+            pela instauração dos novos TACs. ACPs e investigações, totalizando
+            <span style={{ fontWeight: 'bold' }}>{` ${movements} movimentos `}</span>
+            em prol da sociedade nos últimos dias.
+          </p>
+        </div>
+      </article>
+    );
+  }
 }
+
+export default PerformanceRadar;

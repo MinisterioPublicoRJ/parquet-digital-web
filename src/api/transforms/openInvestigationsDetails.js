1b13d91d68a82c9bd5e7000ddd8d00bd644ad4d5
LaylaCsAvila <LaylaCsAvila@users.noreply.github.com>
2020-03-06 18:27:20 -0300

    Revert "Feature/today refactor"

--- src/api/transforms/openInvestigationsDetails.js
+++
@@ -1,9 +0,0 @@
-const openCasesDetailsTransform = ({ variacao_acervo = 0, top_n = [] }) => ({
-  collectionVariation30Days: variacao_acervo,
-  topProsecutors: top_n.map(p => ({
-    name: p.nm_promotoria,
-    collectionVariation30Days: p.variacao_acervo,
-  })),
-});
-
-export default openCasesDetailsTransform;
--- src/api/transforms/todayEntries.js
+++
@@ -1,7 +0,0 @@
-const todayEntriesTransform = ({ hout = 0, lout = 0, nr_entradas_hoje = 0 }) => ({
-  hout,
-  lout,
-  numEntries: nr_entradas_hoje,
-});
-
-export default todayEntriesTransform;

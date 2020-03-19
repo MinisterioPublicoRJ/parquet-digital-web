f9fe52dc073d92f56c2b1b0ab11ce3e7fe15d6c6
Leonardo Alberto Souza <leobetosouza@gmail.com>
2020-03-19 08:23:30 -0300

    finishing loading state on page display component

--- src/pageDisplay/index.jsx
+++ src/pageDisplay/index.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-no-bind */
+
+// TODO: this component deserve more love, docs and abstractions
+// FIXME: remove the comment that disables eslint react/jsx-no-bind
 import React from 'react';
-import { HashRouter, Route, useHistory } from 'react-router-dom';
+import Typist from 'react-typist';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Router from '../router';
 import Today from '../pages/Today/';
@@ -11,6 +15,8 @@ import Progress from '../pages/Progress';
 import SuccessIndicators from '../pages/SuccessIndicators';
 import Decisions from '../pages/Decisions';
 import Loader from '../loader';
+import Api from '../api';
+import { getUser } from '../user';
 
 import { MainTitle, ChangeModeButton } from '../components';
 import './styles.css';
@@ -19,11 +25,12 @@ class PageDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      greeting: this.getGreetingString(),
+      user: undefined,
       isCompact: false,
 
-      isLoading: true,
+      isLogging: true,
       hasAnimateDone: false,
+      loginError: false,
 
       homeLoaded: false,
       yourDeskLoaded: false,
@@ -34,25 +41,46 @@ class PageDisplay extends React.Component {
     this.loadResources();
   }
 
-  /**
-   * [TO BE CHANGED IN THE FUTURE] just returns the greeting to be shown on the page, obeying the rules we'll set
-   * @return {string} [description]
-   */
-  getGreetingString() {
-    return 'Olá Dr. Sidney, bom dia! ';
-  }
-
   setExternalResourcesLoaded(resource) {
     this.setState({ [`${resource}Loaded`]: true });
   }
 
-  animateDone() {
-    this.setState({ hasAnimateDone: true });
+  /**
+   * Returns the greeting to be shown on the page
+   * @return {string} [description]
+   */
+  getGreeting() {
+    const { user } = this.state;
+
+    if (user) {
+      const hours = new Date().getHours();
+
+      if (hours < 12) return `Olá, ${user}, bom dia!`;
+
+      if (hours > 17) return `Olá, ${user}, boa noite!`;
+
+      return `Olá, ${user}, boa tarde!`;
+    }
+
+    return undefined;
   }
 
   async loadResources() {
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    this.setState({ isLoading: false });
+    return this.login();
+  }
+
+  async login() {
+    try {
+      await Api.login(window.localStorage.getItem('access_token'));
+      return this.setState({ isLogging: false, user: getUser().nome });
+    } catch (e) {
+      console.error('PageDisplay#login error', e);
+      return this.setState({ isLogging: false, loginError: true });
+    }
+  }
+
+  animateDone() {
+    this.setState({ hasAnimateDone: true });
   }
 
   /**
@@ -64,21 +92,63 @@ class PageDisplay extends React.Component {
   }
 
   renderLoader() {
-    const { isLoading, hasAnimateDone } = this.state;
+    const { isLogging, hasAnimateDone, loginError, homeLoaded, yourDeskLoaded } = this.state;
+
+    if (loginError) return null;
 
     return (
-      (isLoading || !hasAnimateDone) && <Loader handleAnimateDone={() => this.animateDone()} />
+      (isLogging || !hasAnimateDone || !homeLoaded || !yourDeskLoaded) && (
+        <div className="loading-wrapper">
+          <Loader handleAnimateDone={() => this.animateDone()} />
+        </div>
+      )
     );
   }
 
-  renderPorraToda() {
-    const { greeting, isCompact } = this.state;
+  renderLoginError() {
+    const { loginError } = this.state;
+
+    if (loginError)
+      return (
+        <div className="loading-wrapper">
+          <Typist
+            // TODO: abstract tis to a generic component
+            className="loading-page"
+            // FIXME: find why cursor animation isnt working
+            cursor={{
+              show: true,
+              blink: false,
+              element: '_',
+              hideWhenDone: false,
+              hideWhenDoneDelay: 1000,
+            }}
+          >
+            <p className="paragraphWrapper">
+              &gt; Infelizmente houve um problema com o login, tente recarregar a página ou entre em
+              contato com o suporte. =[
+            </p>
+
+            <p className="paragraphWrapper">&gt; _</p>
+          </Typist>
+        </div>
+      );
+
+    return null;
+  }
+
+  renderPromotron() {
+    const { isCompact, isLogging, loginError } = this.state;
+
+    if (isLogging || loginError) return null;
+
+    const greeting = this.getGreeting();
 
     return (
       <div className="outerView">
         <div className="mainView">
           <div className="headerView">
-            <MainTitle value={greeting} />
+            <MainTitle value={greeting || 'Bom dia, Dr. Sydney, seja bem-vindo.'} />
+            {/* FIXME:remove this hardcoded string when login is ready ^ */}
           </div>
 
           {isCompact && (
@@ -158,7 +228,8 @@ class PageDisplay extends React.Component {
     return (
       <>
         {this.renderLoader()}
-        {this.renderPorraToda()}
+        {this.renderLoginError()}
+        {this.renderPromotron()}
       </>
     );
   }

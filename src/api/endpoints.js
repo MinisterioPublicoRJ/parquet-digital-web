6aa3de4a81c4857d15f75884c090eb9898b201d6
Leonardo Alberto Souza <leobetosouza@gmail.com>
2020-03-19 08:14:51 -0300

    creating login endpoint and updating other endpoint to user orgao

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,20 +1,24 @@
 import t from 'lodash.template';
 
+// TODO: add _URL suffix to all constants
+
 export const BASE_URL = `${process.env.REACT_APP_BASE_URL}/dominio`;
 
-export const TODAY_OUT = t(`${BASE_URL}/saidas/\${id}`);
-export const TODAY_OUTLIERS = t(`${BASE_URL}/outliers/\${id}/\${date}`);
-export const TODAY_ENTRIES = t(`${BASE_URL}/entradas/\${id}/\${cpf}`);
+export const LOGIN_URL = `${BASE_URL}/token/login`;
 
-export const OPEN_CASES_URL = t(`${BASE_URL}/suamesa/vistas/\${id}/\${cpf}`);
-export const OPEN_INVESTIGATIONS_URL = t(`${BASE_URL}/suamesa/investigacoes/\${id}`);
-export const COURT_CASES_URL = t(`${BASE_URL}/suamesa/processos/\${id}`);
-export const CLOSED_CASES_URL = t(`${BASE_URL}/suamesa/finalizados/\${id}`);
+export const TODAY_OUT = t(`${BASE_URL}/saidas/\${orgao}`);
+export const TODAY_OUTLIERS = t(`${BASE_URL}/outliers/\${orgao}/\${date}`);
+export const TODAY_ENTRIES = t(`${BASE_URL}/entradas/\${orgao}/\${cpf}`);
 
-export const OPEN_CASES_DETAILS_URL = t(`${BASE_URL}/suamesa/detalhe/vistas/\${id}/\${cpf}`);
-export const COURT_CASES_DETAILS_URL = t(`${BASE_URL}/suamesa/detalhe/processos/\${id}`);
+export const OPEN_CASES_URL = t(`${BASE_URL}/suamesa/vistas/\${orgao}/\${cpf}`);
+export const OPEN_INVESTIGATIONS_URL = t(`${BASE_URL}/suamesa/investigacoes/\${orgao}`);
+export const COURT_CASES_URL = t(`${BASE_URL}/suamesa/processos/\${orgao}`);
+export const CLOSED_CASES_URL = t(`${BASE_URL}/suamesa/finalizados/\${orgao}`);
+
+export const OPEN_CASES_DETAILS_URL = t(`${BASE_URL}/suamesa/detalhe/vistas/\${orgao}/\${cpf}`);
+export const COURT_CASES_DETAILS_URL = t(`${BASE_URL}/suamesa/detalhe/processos/\${orgao}`);
 export const OPEN_INVESTIGATIONS_DETAILS_URL = t(
-  `${BASE_URL}/suamesa/detalhe/investigacoes/\${id}`,
+  `${BASE_URL}/suamesa/detalhe/investigacoes/\${orgao}`,
 );
 
-export const OPEN_CASES_LIST = t(`${BASE_URL}/suamesa/lista/vistas/\${id}/\${cpf}/\${list}`);
+export const OPEN_CASES_LIST = t(`${BASE_URL}/suamesa/lista/vistas/\${orgao}/\${cpf}/\${list}`);

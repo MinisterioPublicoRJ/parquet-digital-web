3738d9523c731f4f98d4a16b79a694d7697404c6
Layla <laylacsa@msn.com>
2020-03-19 16:45:42 -0300

    Adding mock data for testing

--- src/api/index.js
+++ src/api/index.js
@@ -111,38 +111,39 @@ const Api = (() => {
   }
 
   async function getRadarData(id) {
-    // const { data } = await axios.get(RADAR_DATA({ id }));
-    const data = {
-      pacote_atribuicao: 'Tutela Coletiva - Meio Ambiente',
-      orgao_id: 400552.0,
-      nr_arquivamentos: 179,
-      nr_indeferimentos: 25,
-      nr_instauracoes: 12,
-      nr_tac: 0,
-      nr_acoes: 0,
-      dt_calculo: '2020-03-09T14:50:11.324000',
-      codigo_pacote: 28,
-      max_pacote_arquivamentos: 278,
-      max_pacote_indeferimentos: 151,
-      max_pacote_instauracoes: 65,
-      max_pacote_tac: 10,
-      max_pacote_acoes: 21,
-      med_pacote_aquivamentos: 149,
-      med_pacote_tac: 3,
-      med_pacote_indeferimentos: 70,
-      med_pacote_instauracoes: 32,
-      med_pacote_acoes: 4,
-      perc_arquivamentos: 64.38848920863309,
-      perc_tac: 0.0,
-      perc_indeferimentos: 16.55629139072848,
-      perc_instauracoes: 18.461538461538463,
-      perc_acoes: 0.0,
-      var_med_arquivamentos: 0.20134228187919462,
-      var_med_tac: 0.0,
-      var_med_indeferimentos: -0.6428571428571429,
-      var_med_instauracoes: -0.625,
-      var_med_acoes: 0.0,
-    };
+    const { data } = await axios.get(RADAR_DATA({ id }));
+    // EM CASO DE PROBLEMAS COM A AUTENTICAÇAO, USE ESSE MOCK
+    // const data = {
+    //   pacote_atribuicao: 'Tutela Coletiva - Meio Ambiente',
+    //   orgao_id: 400552.0,
+    //   nr_arquivamentos: 179,
+    //   nr_indeferimentos: 25,
+    //   nr_instauracoes: 12,
+    //   nr_tac: 0,
+    //   nr_acoes: 0,
+    //   dt_calculo: '2020-03-09T14:50:11.324000',
+    //   codigo_pacote: 28,
+    //   max_pacote_arquivamentos: 278,
+    //   max_pacote_indeferimentos: 151,
+    //   max_pacote_instauracoes: 65,
+    //   max_pacote_tac: 10,
+    //   max_pacote_acoes: 21,
+    //   med_pacote_aquivamentos: 149,
+    //   med_pacote_tac: 3,
+    //   med_pacote_indeferimentos: 70,
+    //   med_pacote_instauracoes: 32,
+    //   med_pacote_acoes: 4,
+    //   perc_arquivamentos: 64.38848920863309,
+    //   perc_tac: 0.0,
+    //   perc_indeferimentos: 16.55629139072848,
+    //   perc_instauracoes: 18.461538461538463,
+    //   perc_acoes: 0.0,
+    //   var_med_arquivamentos: 0.20134228187919462,
+    //   var_med_tac: 0.0,
+    //   var_med_indeferimentos: -0.6428571428571429,
+    //   var_med_instauracoes: -0.625,
+    //   var_med_acoes: 0.0,
+    // };
 
     return radarTransform(data);
   }

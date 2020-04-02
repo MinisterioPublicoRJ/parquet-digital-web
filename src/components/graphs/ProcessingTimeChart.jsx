2dcd8d3f080536136131efa6acb512849184435e
re-andrade1987 <renataandrade1987@hotmail.com>
2020-04-02 00:29:12 -0300

    ajusting graphic

--- src/components/graphs/ProcessingTimeChart.jsx
+++ src/components/graphs/ProcessingTimeChart.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { VictoryPie, VictoryPolarAxis } from 'victory';
+import { VictoryPie, VictoryLabel, VictoryTooltip } from 'victory';
 
-function generateGraph(axisExpansion) {
-  return [{ time: 'time', label: `arquivamentos\n${axisExpansion.archives}` }];
-}
-
-function TempoTramitacaoChart({ data, axis }) {
-  const xAxis = generateGraph(axis);
-  
+function TempoTramitacaoChart() {
   // TODO: animate VictoryPie
   return (
     <>
-      <VictoryPie
-        colorScale={['FC2A61 ', '6D86EC', '40B8D9 ', '40B8D9']}
-        innerRadius={140}
-        endAngle={90}
-        startAngle={-90}
-        height={400}
-        styles={{ labels: { fontSize: 20 } }}
-      />
+      <svg width={180} height={180}>
+        <circle cx={100} cy={100} r={30} fill="#c43a31" />
+        <VictoryPie
+          standalone={false}
+          width={200}
+          height={200}
+          innerRadius={65}
+          endAngle={90}
+          startAngle={-90}
+          colorScale={['FC2A61 ', '6D86EC', '40B8D9 ', '40B8D9']}
+        />
+      </svg>
     </>
   );
 }

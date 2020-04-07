3824b107efe4ac64faa5a2551683d72188a66bda
Layla <laylacsa@msn.com>
2020-04-07 16:08:06 -0300

    Clean Data Ready

--- src/pages/ProcessingTime/index.jsx
+++ src/pages/ProcessingTime/index.jsx
@@ -10,18 +10,40 @@ import PinVermelho from '../../assets/svg/pinVermelho';
 
 const ProcessingTime = () => {
   const [processingTime, setProcessingTime] = useState({});
+  const [chartData, setChartData] = useState(null);
+
+  const cleanChartData = raw => {
+    const { min, max, average } = raw.pacoteData;
+
+    // calculating the points exactly between the values
+    const halfMinAvg = (average - min) / 2;
+    const halfMaxAvg = (max - average) / 2;
+
+    // using the midpoints, make three sections to draw "good", "average" and "bad" time ranges
+    const pieData = [
+      // 'good' section from min until halfway to average
+      { x: 1, y: halfMaxAvg - halfMinAvg, color: '#42DCA7' },
+      // 'average' section, from the end of the last section until halfway to max
+      { x: 2, y: halfMaxAvg - halfMinAvg, color: '#6D86EC' },
+      // 'bad' section, from the last section all the way to max
+      { x: 3, y: max - halfMaxAvg, color: '#A256BA' },
+    ];
+
+    const points = null;
+    setChartData({ pieData, points });
+  };
 
   useEffect(() => {
     const loadData = async () => {
       const response = await Api.getProcessingTimeData(getUser());
       setProcessingTime(response);
-      console.log(response);
+      cleanChartData(response);
     };
     loadData();
   }, []);
 
-  if (!processingTime.meta) {
-    return <div>loading</div>
+  if (!processingTime.meta || !chartData) {
+    return <div>loading</div>;
   }
 
   return (
@@ -31,34 +53,32 @@ const ProcessingTime = () => {
         <p>
           Avaliei que o período de tramitação de processos na sua promotoria
           <strong> está mais rápido que a média da casa </strong>
-          entre aquelas de mesma atribuição.<strong>Muito Bom</strong>
+          entre aquelas de mesma atribuição.
+          <strong>Muito Bom</strong>
         </p>
       </div>
       <div className="processingTimeChart" />
-      <ProcessingTimeChart data={processingTime} />
+      <ProcessingTimeChart data={chartData.pieData} />
       <div />
       <div className="main-box-time">
         <div className="second-box-time">
           <PinPreto />
           <div className="third-box-time">
-            <h3 style={{ color: '#474757' }}>
-              {processingTime.orgaoData.min} dias</h3>
+            <h3 style={{ color: '#474757' }}>{processingTime.orgaoData.min} dias</h3>
             <p>Transito mais rápido da sua promotoria</p>
           </div>
         </div>
         <div className="second-box-time">
           <PinAzul />
           <div className="third-box-time">
-            <h3 style={{ color: '#56E8E1' }}>
-            {processingTime.orgaoData.max} dias</h3>
+            <h3 style={{ color: '#56E8E1' }}>{processingTime.orgaoData.max} dias</h3>
             <p>Transito mais lento da sua promotoria</p>
           </div>
         </div>
         <div className="second-box-time">
           <PinVermelho />
           <div className="third-box-time">
-            <h3 style={{ color: '#FF0086' }}>
-            {processingTime.orgaoData.average} dias</h3>
+            <h3 style={{ color: '#FF0086' }}>{processingTime.orgaoData.average} dias</h3>
             <p>Transito médio da sua promotoria</p>
           </div>
         </div>

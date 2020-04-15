13a9ba08e90c032a7ddf11dd6a722fa1ded53c38
Layla <laylacsa@msn.com>
2020-04-15 11:39:18 -0300

    Fixing accidentally deleted styles

--- src/components/YourTable/index.js
+++ src/components/YourTable/index.js
@@ -1,10 +1,4 @@
-// export { default as CourtCasesTab } from './CourtCasesTab';
-// export { default as OpenCasesTab } from './OpenCasesTab';
-// export { default as OpenInvestigationsTab } from './OpenInvestigationsTab';
-// export { default as TabControl } from './TabControl';
-
 export { default as ControlButton } from './ControlButton';
-export { default as CasesIndicator } from './CasesIndicator';
 
 export { default as OpenCasesDetail } from './openCasesDetail';
 export { default as OpenInvestigationsDetail } from './openInvestigationsDetail';

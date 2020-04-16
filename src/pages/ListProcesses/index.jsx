560f48396a796ce1f98fec776671649f7df28a8e
re-andrade1987 <renataandrade1987@hotmail.com>
2020-04-16 18:27:37 -0300

    Creating componet ListProcesses

---
+++ src/pages/ListProcesses/index.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+import './styles.css';
+import Api from '../../api';
+import { getUser } from '../../user';
+
+const ListProcesses = () => {
+
+  return (
+    <article className="page-lista-processos">
+     
+    </article>
+  );
+};
+
+export default ListProcesses;

953fbac4c8b009e48cd1f70fa06af0f493441d93
Layla <laylacsa@msn.com>
2020-04-16 20:17:13 -0300

    Base of the graph ready

--- src/components/graphs/ProcessingTimeChart.jsx
+++ src/components/graphs/ProcessingTimeChart.jsx
@@ -1,61 +1,85 @@
 import React, { useState, useEffect } from 'react';
-import { VictoryPie, VictoryLabel, VictoryChart, VictoryScatter } from 'victory';
+import { VictoryPie, VictoryLabel, VictoryChart, VictoryScatter, VictoryAxis } from 'victory';
 import ChartPointComponent from '../../pages/ProcessingTime/chartPointComponent';
+import CHART_THEME from '../../themes/chartThemes';
 
 const graphicColorMain = ['#A256BA', '#56E8E1', '#A256BA', '#42DCA7']; // Colors
 const graphicColor = ['#F8F9FB']; // Colors
-const wantedGraphicData = [{ y: 310 }, { y: 350 }, { y: 740 }, { y: 120 }]; // Data that we want to display
+// const wantedGraphicData = [{ y: 310 }, { y: 350 }, { y: 740 }, { y: 120 }]; // Data that we want to display
 
-function TempoTramitacaoChart() {
+function TempoTramitacaoChart({ data, scatter, labelText, domain }) {
+  const { min, max } = domain;
+  console.log('domain', min, max, scatter);
   // TODO: animate VictoryPie
-  const [graphicData, setGraphicData] = useState(wantedGraphicData);
+  // const [graphicData, setGraphicData] = useState(data);
+  //
+  // useEffect(() => {
+  //   setGraphicData(wantedGraphicData); // Setting the data that we want to display
+  // }, []);
 
-  useEffect(() => {
-    setGraphicData(wantedGraphicData); // Setting the data that we want to display
-  }, []);
+  const victoryChartSettings = {
+    domain: { x: [min, max], y: [0, 100] },
+    endAngle: 200,
+    height: 200,
+    padding: { top: 20, bottom: -20, left: 0, right: 0 },
+    standalone: false,
+    startAngle: 10,
+    style: {
+      parent: {
+        maxHeight: '100%',
+        padding: 0,
+        margin: 0,
+      },
+    },
+    width: 200,
+  };
+
+  const decorationPieSettings = {
+    colorScale: graphicColor,
+    endAngle: 120,
+    innerRadius: 50,
+    labels: () => null,
+    opacity: 1,
+    padding: 5,
+    startAngle: -120,
+    style: {
+      data: { fill: '#F4F5FA' },
+    }
+  };
+
+  const chartPieSettings = {
+    endAngle: 80,
+    innerRadius: 90,
+    height: 200,
+    labelPosition: 'endAngle',
+    radius: 100,
+    startAngle: -110,
+    padAngle: 2,
+    sortKey: 'x',
+    style: {
+      labels: { fontSize: 15, fontWeight: '400', height: 10, fill: ({ datum }) => datum.color },
+      data: { fill: ({ datum }) => datum.color },
+    },
+  };
+
+  const scatterSettings = {
+    style: { data: { fill: '#c43a31' } },
+  };
+
   return (
-    <>
-      <svg width={380} height={300}>
-        <VictoryChart width={300} height={300} standalone={false}>
-          <VictoryPie
-            width={480}
-            height={450}
-            innerRadius={50}
-            endAngle={120}
-            startAngle={-120}
-            colorScale={graphicColor}
-            opacity={0.1}
-            padding={5}
-          />
-          <circle
-            cx="150"
-            cy="150"
-            r="40"
-            fill="none"
-            stroke="#B3B3B3"
-            style={{ fontSize: 20, fontWeight: 'bold' }}
-          />
-          <VictoryLabel textAnchor="middle" x={150} y={150} text="620" />
-          <VictoryScatter data={graphicData} dataComponent={<ChartPointComponent />} />
-          <VictoryPie
-            width={380}
-            height={400}
-            innerRadius={110}
-            endAngle={80}
-            startAngle={-110}
-            padAngle={2}
-            opacity={0.1}
-            colorScale={graphicColorMain}
-            padding={5}
-            data={graphicData}
-            labels={({ datum }) => datum.y}
-            style={{
-              labels: { fontSize: 15, fontWeight: '400', height: 10 },
-            }}
-          />
-        </VictoryChart>
-      </svg>
-    </>
+    <svg width="100%" height="100%" viewBox="0 0 200 200">
+      <VictoryChart polar {...victoryChartSettings}>
+        {/* DECORATIONS */}
+        <circle cx="100" cy="140" r="40" fill="none" stroke="#B3B3B3" />
+        <VictoryLabel textAnchor="middle" x={100} y={140} text={labelText} />
+        <VictoryPie {...decorationPieSettings} />
+
+        {/* GRAPHS AND AXIS */}
+        <VictoryAxis style={{ axis: { stroke: 'none' } }} />
+        <VictoryPie {...chartPieSettings} data={data} />
+        <VictoryScatter data={scatter} {...scatterSettings} />
+      </VictoryChart>
+    </svg>
   );
 }
 export default TempoTramitacaoChart;
--- src/pages/ProcessingTime/index.jsx
+++ src/pages/ProcessingTime/index.jsx
@@ -7,14 +7,15 @@ import { ProcessingTimeChart } from '../../components/graphs';
 import PinAzul from '../../assets/svg/pinAzul';
 import PinPreto from '../../assets/svg/pinPreto';
 import PinVermelho from '../../assets/svg/pinVermelho';
-import ChartPointComponent from '../../pages/ProcessingTime/chartPointComponent'
+import ChartPointComponent from '../../pages/ProcessingTime/chartPointComponent';
 const ProcessingTime = () => {
   const [processingTime, setProcessingTime] = useState({});
   const [chartData, setChartData] = useState(null);
 
   const cleanChartData = raw => {
+    const organAvg = raw.orgaoData.average.toFixed(2);
     const { min, max, average } = raw.pacoteData;
-    console.log('min, max, average', min, max, average);
+    const domain = { min, max };
 
     // calculating the points exactly between the values
     const halfMinAvg = (average - min) / 2;
@@ -23,15 +24,19 @@ const ProcessingTime = () => {
     // using the midpoints, make three sections to draw "good", "average" and "bad" time ranges
     const pieData = [
       // 'good' section from min until halfway to average
-      { x: 2, y: halfMinAvg / max, color: '#42DCA7' },
+      { x: 2, y: halfMinAvg / max, color: '#42DCA7', label: halfMinAvg.toFixed(0) },
       // 'average' section, from the end of the last section until halfway to max
-      { x: 1, y: (halfMaxAvg - halfMinAvg) / max, color: '#6D86EC' },
+      { x: 1, y: (halfMaxAvg - halfMinAvg) / max, color: '#6D86EC', label: halfMaxAvg.toFixed(0) },
       // 'bad' section, from the last section all the way to max
-      { x: 0, y: (max - halfMaxAvg) / max, color: '#A256BA' },
+      { x: 0, y: (max - halfMaxAvg) / max, color: '#A256BA', label: max.toFixed(0) },
     ];
 
-    const points = null;
-    setChartData({ pieData, points });
+    const points = [
+      { x: min, y: 100, type: 'min' },
+      { x: average, y: 100, type: 'average' },
+      { x: max, y: 100, type: 'max' },
+    ];
+    setChartData({ pieData, points, domain, organAvg });
   };
 
   useEffect(() => {
@@ -46,50 +51,65 @@ const ProcessingTime = () => {
   if (!processingTime.meta || !chartData) {
     return <div>loading</div>;
   }
+  // return (
+  //   <article className="page-tramitacao">
+  //     <div className="tramitacao-texts">
+  //       <ChartPointComponent />
+  //       <h3>Tempo de tramitação</h3>
+  //       <p>
+  //         Avaliei que o período de tramitação de processos na sua promotoria
+  //         <strong> está mais rápido que a média da casa </strong>
+  //         entre aquelas de mesma atribuição.
+  //         <strong>Muito Bom</strong>
+  //       </p>
+  //     </div>
+  //     <div className="processingTimeChart" />
+  //     <ProcessingTimeChart data={chartData.pieData} />
+  //     <div />
+  //     <div className="main-box-time">
+  //       <div className="second-box-time">
+  //         <div className="pin">
+  //           <PinPreto />
+  //         </div>
+  //         <div className="third-box-time">
+  //           <h3 style={{ color: '#474757' }}>{processingTime.orgaoData.min} dias</h3>
+  //           <p>Transito mais rápido da sua promotoria</p>
+  //         </div>
+  //       </div>
+  //       <div className="second-box-time">
+  //        <div className="pin">
+  //           <PinAzul />
+  //        </div>
+  //         <div className="third-box-time">
+  //           <h3 style={{ color: '#56E8E1' }}>{processingTime.orgaoData.max} dias</h3>
+  //           <p>Transito mais lento da sua promotoria</p>
+  //         </div>
+  //       </div>
+  //       <div className="second-box-time">
+  //       <div className="pin">
+  //         <PinVermelho />
+  //       </div>
+  //         <div className="third-box-time">
+  //           <h3 style={{ color: '#FF0086' }}>{processingTime.orgaoData.average} dias</h3>
+  //           <p>Transito médio da sua promotoria</p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </article>
+  // );
+
   return (
     <article className="page-tramitacao">
-      <div className="tramitacao-texts">
-        <ChartPointComponent />
-        <h3>Tempo de tramitação</h3>
-        <p>
-          Avaliei que o período de tramitação de processos na sua promotoria
-          <strong> está mais rápido que a média da casa </strong>
-          entre aquelas de mesma atribuição.
-          <strong>Muito Bom</strong>
-        </p>
+      <div className="pt-Texts">a</div>
+      <div className="pt-graph">
+        <ProcessingTimeChart
+          data={chartData.pieData}
+          scatter={chartData.points}
+          domain={chartData.domain}
+          labelText={chartData.organAvg}
+        />
       </div>
-      <div className="processingTimeChart" />
-      <ProcessingTimeChart data={chartData.pieData} />
-      <div />
-      <div className="main-box-time">
-        <div className="second-box-time">
-          <div className="pin">
-            <PinPreto />
-          </div>
-          <div className="third-box-time">
-            <h3 style={{ color: '#474757' }}>{processingTime.orgaoData.min} dias</h3>
-            <p>Transito mais rápido da sua promotoria</p>
-          </div>
-        </div>
-        <div className="second-box-time">
-         <div className="pin">
-            <PinAzul />
-         </div>
-          <div className="third-box-time">
-            <h3 style={{ color: '#56E8E1' }}>{processingTime.orgaoData.max} dias</h3>
-            <p>Transito mais lento da sua promotoria</p>
-          </div>
-        </div>
-        <div className="second-box-time">
-        <div className="pin">
-          <PinVermelho />
-        </div>
-          <div className="third-box-time">
-            <h3 style={{ color: '#FF0086' }}>{processingTime.orgaoData.average} dias</h3>
-            <p>Transito médio da sua promotoria</p>
-          </div>
-        </div>
-      </div>
+      <div className="pt-mainBox">a</div>
     </article>
   );
 };

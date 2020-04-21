20bfe8059be56e3487260860ab3735e86c0b196e
re-andrade1987 <renataandrade1987@hotmail.com>
2020-04-21 17:05:58 -0300

    finalized list of processes component

--- src/pages/ListProcesses/index.jsx
+++ src/pages/ListProcesses/index.jsx
@@ -4,7 +4,6 @@ import './styles.css';
 import Api from '../../api';
 import { Table, SectionTitle } from '../../components';
 import { getUser } from '../../user';
-// import TableProcesses from '../../components/TableProcesses';
 
 const ListProcesses = () => {
   // eslint-disable-next-line no-shadow
@@ -22,7 +21,6 @@ const ListProcesses = () => {
     const loadData = async () => {
       const response = await Api.getListProcesses(getUser());
       setListProcesses(response);
-      // console.log('listProcesses', response);
     };
     loadData();
   }, []);
@@ -30,14 +28,11 @@ const ListProcesses = () => {
   if (!listProcesses) {
     return <div>loading</div>;
   }
-  
   return (
     <div className="page-lista-processos">
-      {/*<h3>Lista de processos</h3>*/}
       <SectionTitle value="Lista de Processos" />
       <div className="lista-processos">
         <Table data={listProcesses} columns={tableColumns} showHeader />
-        {/*<TableProcesses list={listProcesses} data={data} />*/}
       </div>
     </div>
   );

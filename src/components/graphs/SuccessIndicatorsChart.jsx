d1b8b143b9edb403d76efc61b42eb41364af2451
re-andrade1987 <renataandrade1987@hotmail.com>
2020-05-07 15:02:22 -0300

    Ajusting graphic

--- src/components/graphs/SuccessIndicatorsChart.jsx
+++ src/components/graphs/SuccessIndicatorsChart.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { VictoryBar } from 'victory';
+import { VictoryBar, VictoryStack, VictoryChart } from 'victory';
 
 function SuccessIndicatorsChart() {
+  const data = [
+    {
+      user: 'John',
+      apples: 50,
+      oranges: 10,
+    },
+  ];
+
+  const chartStyle = {
+    style: {
+      data: { fill: 'rgb(248, 108, 114)' },
+      height: 100,
+    },
+  };
+  const chartStyleSecond = {
+    style: {
+      data: { fill: '#F8F9FB' },
+    },
+  };
+  const victoryChartSettings = {
+    padding: { top: 40, bottom: 320 },
+  };
+
   return (
     <div className="box-chart-success">
-      <p style={{fontSize: 20}}>Sua Taxa de <strong>Resolutividade</strong></p>
+      <p style={{ fontSize: 20 }}>Sua Taxa de <strong> Resolutividade </strong></p>
       <p>[Denuncias + arquivamentos + acordo/vistas abertas]</p>
-        <VictoryBar maxDomain={{ x: 1 }} horizontal style={{ data: { fill: '#c43a31' } }} />
+      <VictoryChart {...victoryChartSettings}>
+        <VictoryStack {...chartStyle} maxDomain={{ x: 1 }} horizontal>
+          <VictoryBar {...chartStyle} maxDomain={{ x: 1 }} data={data} x="user" y="oranges" />
+          <VictoryBar {...chartStyleSecond} maxDomain={{ x: 1 }} data={data} x="user" y="oranges" />
+        </VictoryStack>
+      </VictoryChart>
     </div>
   );
 }

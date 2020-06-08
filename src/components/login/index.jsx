493e8cf23ae4770d385c2e7de412b5ea192eabb4
renata <renataandrade1987@hotmail.com>
2020-06-08 17:35:24 -0300

    initial commit login screen

---
+++ src/components/login/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+import './styles.css';
+import Api from '../../api';
+import { getUser } from '../../user';
+
+const Login = () => {
+  // eslint-disable-next-line no-shadow
+  const [login, setLogin] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  return (
+    <div className="login">
+     
+    </div>
+  );
+};
+
+export default ProcessList;

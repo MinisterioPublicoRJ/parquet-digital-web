fd921a2ca15ade14f906f1a37f157069ea67b292
renata <renataandrade1987@hotmail.com>
2020-06-16 01:47:26 -0300

    Exibindo dados corretos dos alertas

--- src/sections/Alerts/index.js
+++ src/sections/Alerts/index.js
@@ -59,143 +59,22 @@ class Alerts extends React.Component {
     }
   }
 
-  /**
+   /**
    * Finds the details for each alert type
    * @param  {json} alert {alertCode}
    * @return {json}       { icon: node, message: node, action: null, actionLink: null, background: string }
    */
-  cleanAlert(alert) {
-    // this will be completed for all alert types later
-    let icon = null;
-    let message = null;
-    const action = null;
-    const actionLink = null;
-    let background = null;
 
-    switch (alert.alertCode) {
-      // ALERTAS DA TUTELA
-      case 'DCTJ':
-        icon = <Tjrj />;
-        message = (
-          <span>
-            O <strong> processo criminal {` ${` ${alert.docNum} `} `}</strong> está no TJRJ há <strong>mais de 60 dias</strong> sem retorno.
-          </span>
-        );
-        background = '#F86C72';
-        break;
+    cleanAlert(alert){
+      let icon = null;
+      let message = null;
+      const action = null;
+      const actionLink = null;
+      let background = null;
 
-      case 'DNTJ':
-        icon = <Tjrj />;
-        message = (
-          <span>
-            O <strong>processo não criminal {` ${alert.docNum} `}</strong> está no TJRJ há <strong>mais de 120 dias</strong> sem retorno.
-          </span>
-        );
-        background = '#F86C72';
-        break;
+      switch(alert.alertCode){
 
-      case 'DT2I':
-        icon = <Home />;
-        message = (
-          <span>
-            O <strong>orgão responsável</strong> pelo processo <strong>{` ${alert.docNum} `}</strong> está possivelmente <strong> desatualizado</strong>.
-          </span>
-        );
-        background = '#5C6FD9';
-        break;
-
-      case 'MVVD':
-        icon = <Ouvidoria />;
-        message = (
-          <span>
-            O processo <strong> {` ${alert.docNum} `}</strong> possui <strong>vitimas recorrentes</strong> de violência domestica.
-          </span>
-        );
-        background = '#F86C72';
-        break;
-
-      case 'PA1A':
-        icon = <ClockIcon />;
-        message = (
-          <span>
-            O<strong> processo administrativo {` ${alert.docNum} `} </strong> está aberto <strong>há mais de 1 ano</strong>.
-          </span>
-        );
-        background = '#5C6FD9';
-        break;
-
-      case 'PPFP':
-        icon = <ClockIcon />;
-        message = (
-          <span>
-            O <strong>procedimento preparatório {` ${alert.docNum} `}</strong> está com prazo próximo de vencer.
-          </span>
-        );
-        background = '#f86c72';
-        break;
-
-      case 'IC1A':
-        icon = <ClockIcon />;
-        message = (
-          <span>
-            O inquérito civil ativo <strong> {` ${alert.docNum} `} </strong> <strong> está sem prorrogação </strong> há <strong>mais de 1 ano</strong>.
-          </span>
-        );
-        background = '#f86c72';
-        break;
-
-      case 'NF30':
-        icon = <ClockIcon />;
-        message = (
-          <span>
-            A <strong> noticia de fato{` ${alert.docNum} `}</strong> foi autuada há mais de <strong>120 dias</strong> e ainda está <strong>sem tratamento</strong>
-          </span>
-        );
-        background = '#f86c72';
-        break;
-
-      case 'OFFP':
-        icon = <ClockIcon />;
-        message = (
-          <span>
-            O ofício <strong>{` ${alert.docNum} `}</strong> está com o <strong> prazo de apreciação esgotado </strong>.
-          </span>
-        );
-        background = '#f86c72';
-        break;
-
-      case 'OUVI':
-        icon = <Ouvidoria />;
-        message = (
-          <span>
-            O<strong> expediente de ouvidoria {` ${alert.docNum} `}</strong> foi enviado porém <strong> não recebido</strong>
-          </span>
-        );
-        background = '#5C6FD9';
-        break;
-
-      case 'VADF':
-        icon = <Va />;
-        message = (
-          <span>
-            Você tem uma <strong> vista aberta</strong> no <strong>documento {` ${alert.docNum} `}, sinalizado como fechado</strong>
-          </span>
-        );
-        background = '#28A7E0';
-        break;
-
-        case 'DORD':
-          icon = <Mprj />;
-          message = (
-            <span>
-              O processo <strong> {` ${alert.docNum} `}</strong> está possivelmente <strong> desatualizado</strong>.
-            </span>
-          );
-          background = '#5C6FD9';
-          break;
-
-      // ALERTAS DA PIP
-      case 'GATE':
+        case 'GATE':
         icon = <CorujaGate />;
         message = (
           <span>
@@ -245,22 +124,140 @@ class Alerts extends React.Component {
       //         background = '#F86C72';
       //         break;
 
-      // case 'VADF':
-      //   icon = <Home />;
-      //   message = (
-      //     <span>
-      //       <strong> Movimentação N:{` ${alert.docNum} `} </strong> em processo desta promotoria na segunda
-      //       instância
-      //     </span>
-      //   );
-      //   background = '#5C6FD9';
-      //   break;
-      //
+      default:
+      break;
+    }
+
+    return { icon, message, action, actionLink, background };
+  }
+
+ 
+  cleanAlert(alertsTotal) {
+    // this will be completed for all alert types later
+    let icon = null;
+    let message = null;
+    const action = null;
+    const actionLink = null;
+    let background = null;
+
+    switch (alertsTotal.sigla) {
+      // ALERTAS DA TUTELA
+      case 'DCTJ':
+        icon = <Tjrj />;
+        message = (
+          <span>
+            Há <strong>  {` ${` ${alertsTotal.count} `} `}</strong> processos criminais no TJRJ há <strong>mais de 60 dias</strong> sem retorno.
+          </span>
+        );
+        background = '#F86C72';
+        break;
+
+      case 'DNTJ':
+        icon = <Tjrj />;
+        message = (
+          <span>
+            Há <strong> {` ${alertsTotal.count} `}</strong> processos não criminais no TJRJ há <strong>há mais de 120 dias</strong> sem retorno.
+          </span>
+        );
+        background = '#F86C72';
+        break;
+
+      case 'MVVD':
+        icon = <Ouvidoria />;
+        message = (
+          <span>
+            Há <strong> {` ${alertsTotal.count} `} processos</strong> com <strong> vitimas recorrentes</strong> de <strong> violência domestica </strong>.
+          </span>
+        );
+        background = '#F86C72';
+        break;
+
+      case 'PA1A':
+        icon = <ClockIcon />;
+        message = (
+          <span>
+            Há <strong> {` ${alertsTotal.count} `} </strong> <strong> processos administrativos abertos há mais de 1 ano</strong>.
+          </span>
+        );
+        background = '#5C6FD9';
+        break;
+
+      case 'PPFP':
+        icon = <ClockIcon />;
+        message = (
+          <span>
+            Há <strong> {` ${alertsTotal.count} `}</strong> <strong> procedimentos preparatório </strong> com <strong> prazo de tratamento esgotado.</strong>
+          </span>
+        );
+        background = '#f86c72';
+        break;
+
+      case 'IC1A':
+        icon = <ClockIcon />;
+        message = (
+          <span>
+            Há <strong> {` ${alertsTotal.count} `} </strong> <strong> inquéritos civil </strong> ativo <strong> sem prorrogação </strong> há <strong> mais de 1 ano</strong>.
+          </span>
+        );
+        background = '#f86c72';
+        break;
+
+      case 'NF30':
+        icon = <ClockIcon />;
+        message = (
+          <span>
+            Há <strong> {` ${alertsTotal.count} `}</strong> notícias de fato autuada há mais de <strong>120 dias</strong> e ainda está <strong>sem tratamento</strong>
+          </span>
+        );
+        background = '#f86c72';
+        break;
+
+      case 'OFFP':
+        icon = <ClockIcon />;
+        message = (
+          <span>
+            Há <strong>{` ${alertsTotal.count} `} oficio</strong> com <strong> prazo de apreciação esgotado </strong>.
+          </span>
+        );
+        background = '#f86c72';
+        break;
+
+      case 'OUVI':
+        icon = <Ouvidoria />;
+        message = (
+          <span>
+            <strong> {` ${alertsTotal.count} `}</strong> <strong> Expediente </strong> de <strong> Ouvidoria </strong> enviado porém <strong> não recebido</strong>
+          </span>
+        );
+        background = '#5C6FD9';
+        break;
+
+      case 'VADF':
+        icon = <Va />;
+        message = (
+          <span>
+            Você tem <strong> {` ${alertsTotal.count} `} vistas abertas </strong> em <strong> documentos sinalizados como fechado</strong>
+          </span>
+        );
+        background = '#28A7E0';
+        break;
+
+      // ALERTAS DA PIP
+      case 'GATE':
+        icon = <CorujaGate />;
+        message = (
+          <span>
+            O <strong>Gate </strong>finalizou a <strong>IT</strong> solicitada no procedimento <strong>{` ${alert.docNum} `}</strong>
+          </span>
+        );
+        background = '#374354';
+        break;
+
         case 'DT2I':
              icon = <Home />;
             message = (
              <span>
-                Há <strong>01 processo {` ${alert.docNum} `}</strong> cujo <strong>orgão responsável</strong> está possivelmente<strong> desatualizado</strong>
+                <strong> {` ${alertsTotal.count} `} movimentações </strong> em processo desta promotoria na <strong> segunda instância </strong>
               </span>
               );
               background = '#5C6FD9';
@@ -274,7 +271,7 @@ class Alerts extends React.Component {
   }
 
   render() {
-    const { alerts, loading, errorAlerts } = this.state;
+    const { alerts, alertsTotal, loading, errorAlerts } = this.state;
 
     if (loading) return <aside>Carregando...</aside>;
     return (
@@ -282,11 +279,10 @@ class Alerts extends React.Component {
         <div className="alerts-header">
           <SectionTitle value="central de alertas" />
           <div className="alerts-total">
-            <span>{alerts.length}</span>
           </div>
         </div>
         <div className="alerts-body">
-          {alerts.map((alert, i) => {
+          {alertsTotal.map((alert, i) => {
             const { icon, message, action, actionLink, background } = this.cleanAlert(alert);
             return (
               <AlertBadge

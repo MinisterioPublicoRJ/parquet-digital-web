65604401b46e23ebe8a4940b68c5995b7c83e769
renata <renataandrade1987@hotmail.com>
2020-06-17 13:25:07 -0300

    Retirando console.log

--- src/sections/Today/index.jsx
+++ src/sections/Today/index.jsx
@@ -92,7 +92,6 @@ class Today extends Component {
 
       collectionPhrase = this.analyzeCollection(primQ, terQ, acervoQtd);
       groupName = NOMES_PROMOTORIAS[cod];
-      console.log(primQ, terQ, acervoQtd, cod)
     } catch (e) {
       errorTodayOutliers = true;
     } finally {

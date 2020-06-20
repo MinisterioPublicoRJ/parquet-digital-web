866628c94afe425fa63060ed8405f9e57c2ad231
Layla <laylacsa@msn.com>
2020-06-20 14:23:48 -0300

    Fixing tablet grid

--- src/pages/pip/index.jsx
+++ src/pages/pip/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import '../styles.css';
 import './styles.css';
-import SuccesIndicators from '../../sections/SuccessIndicators';
+import '../styles.css';
 
 import {
-  Alerts,
-  MainInvestigated,
   Today,
-  PerformanceRadar,
-  ProcessingTime,
   YourDesk,
+  Alerts,
+  PerformanceRadar,
+  MainInvestigated,
+  SuccessIndicators,
+  ProcessingTime,
 } from '../../sections';
 
 const propTypes = { userName: PropTypes.string.isRequired };
@@ -19,13 +19,13 @@ const propTypes = { userName: PropTypes.string.isRequired };
 function Pip({ userName, user }) {
   return (
     <div className="base-grid pip-grid">
+      <Today user={user} userName={userName} />
       <YourDesk user={userName} />
       <Alerts user={userName} />
       <PerformanceRadar.Pip user={userName} />
-      <SuccesIndicators user={userName} />
       <MainInvestigated user={userName} />
+      <SuccessIndicators user={userName} />
       <ProcessingTime user={user} />
-      <Today user={user} userName={userName} />
     </div>
   );
 }

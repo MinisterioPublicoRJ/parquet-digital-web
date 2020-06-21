728cce1231822c02336d2759d66f2d5726170cf7
Layla <laylacsa@msn.com>
2020-06-20 21:06:21 -0300

    Tutela titles

--- src/components/YourDesk/Ranking/titleConstants.js
+++ src/components/YourDesk/Ranking/titleConstants.js
@@ -3,6 +3,8 @@ const TITLEDICT = {
   acervo_fim: 'maiores volumes do mês',
   nr_documentos_distintos_atual: 'maiores volumes do mês',
   nr_aproveitamentos_atual: 'maiores aproveitamentos do mês',
+  variacao_acervo: 'Maiores Reduções do Mês',
+  nr_acoes_ultimos_30_dias: 'Recorde de Ações Ajuizadas no Mês',
 };
 
 export default TITLEDICT;

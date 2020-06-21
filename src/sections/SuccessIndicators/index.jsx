5fa0aa568f7f56876b98c264e343f0d1ee015928
Layla <laylacsa@msn.com>
2020-06-21 13:04:34 -0300

    Adding percentage values to tooltips

--- src/sections/SuccessIndicators/index.jsx
+++ src/sections/SuccessIndicators/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { formatPercent } from '../../utils'
+import { formatPercent } from '../../utils';
 import './styles.css';
 import Api from '../../api';
 import { SectionTitle, Spinner } from '../../components';
@@ -15,7 +15,7 @@ const SuccessIndicators = () => {
       setLoading(true);
       try {
         const response = await Api.getsuccessIndicators(getUser());
-        console.log(response)
+        console.log(response);
 
         setSuccessIndicators(response);
       } catch (e) {
@@ -30,9 +30,12 @@ const SuccessIndicators = () => {
   if (loading) {
     return <Spinner size="medium" />;
   }
-  
-  const percent = formatPercent(successIndicators);
-  console.log(percent)
+
+  console.log(successIndicators);
+  // this values will never be lower than zero
+  const resolutividadePercent = formatPercent(successIndicators.pResolutividade[0].y);
+  const elucidacoesPercent = formatPercent(successIndicators.pElucidacoes[0].y);
+  const finalizacoesPercent = formatPercent(successIndicators.pFinalizacoes[0].y);
 
   return (
     <article className="successIndicators-outer">
@@ -42,16 +45,20 @@ const SuccessIndicators = () => {
           <h3>Resolutividade</h3>
           <span>Denúncias + arquivamentos + acordos/vistas abertas("i")</span>
           <div className="first-box-tooltip">
-            <strong>100%</strong>
+            <strong>{resolutividadePercent}</strong>
           </div>
-          <SuccessIndicatorsChart className="chart" data={successIndicators.taxaResolutivdade} color="#F86C72" />
+          <SuccessIndicatorsChart
+            className="chart"
+            data={successIndicators.pResolutividade}
+            color="#F86C72"
+          />
         </div>
 
         <div className="successIndicators-item">
           <h3>Índice de Elucidação - Denúncias.</h3>
           <span>Número de procedimentos com denúncia("i")</span>
           <div className="second-box-tooltip">
-            <strong>30%</strong>
+            <strong>{elucidacoesPercent}</strong>
           </div>
           <SuccessIndicatorsChart data={successIndicators.pElucidacoes} color="#F8BD6C" />
         </div>
@@ -60,9 +67,13 @@ const SuccessIndicators = () => {
           <h3>Índice de Finalização</h3>
           <span>Andamentos que finalizam o processo("i")</span>
           <div className="third-box-tooltip">
-            <strong>40%</strong>
+            <strong>{finalizacoesPercent}</strong>
           </div>
-          <SuccessIndicatorsChart className="chart" data={successIndicators.pFinalizacoes} color="#71D0A4" />
+          <SuccessIndicatorsChart
+            className="chart"
+            data={successIndicators.pFinalizacoes}
+            color="#71D0A4"
+          />
         </div>
       </div>
     </article>

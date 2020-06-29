aa7f71711285a727d2d46c6ec27ee9394f9c04da
Layla <laylacsa@msn.com>
2020-06-29 20:20:04 -0300

    Deleting log

--- src/sections/SuccessIndicators/index.jsx
+++ src/sections/SuccessIndicators/index.jsx
@@ -16,13 +16,12 @@ const SuccessIndicators = () => {
       try {
         const response = await Api.getsuccessIndicators(getUser());
         setSuccessIndicators(response);
-        console.log(response);
       } catch (e) {
         setLoading(true);
       } finally {
         setLoading(false);
       }
-    };  
+    };
     loadData();
   }, []);
 
@@ -31,9 +30,15 @@ const SuccessIndicators = () => {
   }
 
   // this values will never be lower than zero
-  const resolutividadePercent = successIndicators.pResolutividade ? formatPercent(successIndicators.pResolutividade[0].y): '0%';
-  const elucidacoesPercent = successIndicators.pElucidacoes ? formatPercent(successIndicators.pElucidacoes[0].y): '0%';
-  const finalizacoesPercent = successIndicators.pFinalizacoes ? formatPercent(successIndicators.pFinalizacoes[0].y): '0%';
+  const resolutividadePercent = successIndicators.pResolutividade
+    ? formatPercent(successIndicators.pResolutividade[0].y)
+    : '0%';
+  const elucidacoesPercent = successIndicators.pElucidacoes
+    ? formatPercent(successIndicators.pElucidacoes[0].y)
+    : '0%';
+  const finalizacoesPercent = successIndicators.pFinalizacoes
+    ? formatPercent(successIndicators.pFinalizacoes[0].y)
+    : '0%';
 
   return (
     <article className="successIndicators-outer">
@@ -49,30 +54,36 @@ const SuccessIndicators = () => {
           <SuccessIndicatorsChart
             className="chart"
             data={successIndicators.pResolutividade}
-            color={ resolutividadePercent === '0%' ? "#f8f9fb" : "#F86C72" }
+            color={resolutividadePercent === '0%' ? '#f8f9fb' : '#F86C72'}
           />
         </div>
         <div className="successIndicators-item">
           <h3>Índice de Elucidação de Casos</h3>
-          <span>Número de procedimentos com denúncia/Número de procedimentos do Grupo de vistas abertas, no período de 18 a 6 meses atrás</span>
+          <span>
+            Número de procedimentos com denúncia/Número de procedimentos do Grupo de vistas abertas,
+            no período de 18 a 6 meses atrás
+          </span>
           <div className="second-box-tooltip">
             <strong>{elucidacoesPercent}</strong>
           </div>
-          <SuccessIndicatorsChart 
-          data={successIndicators.pElucidacoes}
-          color={ elucidacoesPercent === '0%' ? "#f8f9fb" : "#F8BD6C" }
+          <SuccessIndicatorsChart
+            data={successIndicators.pElucidacoes}
+            color={elucidacoesPercent === '0%' ? '#f8f9fb' : '#F8BD6C'}
           />
         </div>
         <div className="successIndicators-item">
           <h3>Índice de Finalização</h3>
-          <span>Denúncias, arquivamentos, acordos/Número de procedimentos do Grupo de vistas abertas, no período de 18 a 6 meses atrás</span>
+          <span>
+            Denúncias, arquivamentos, acordos/Número de procedimentos do Grupo de vistas abertas, no
+            período de 18 a 6 meses atrás
+          </span>
           <div className="third-box-tooltip">
             <strong>{finalizacoesPercent}</strong>
           </div>
           <SuccessIndicatorsChart
             className="chart"
             data={successIndicators.pFinalizacoes}
-            color={ finalizacoesPercent === '0%' ? "#f8f9fb" : "#71D0A4" }
+            color={finalizacoesPercent === '0%' ? '#f8f9fb' : '#71D0A4'}
           />
         </div>
       </div>

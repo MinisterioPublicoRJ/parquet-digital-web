7a822b32b735d812a6576ee0b0554bd2d1989b44
Layla <laylacsa@msn.com>
2020-07-03 17:45:27 -0300

    Fixing broken import on router

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { Switch, Route, HashRouter, Redirect } from 'react-router-dom';
 
 import { useAuth } from './authContext';
-import Auth from '../app/authContext';
 import Login from '../login';
 import Dashboard from '../dashboard';
-
 
 function Router() {
   const { user } = useAuth();
   return (
     <HashRouter>
       <Switch>
-        <Route exact path="/" component={Auth}>
+        <Route exact path="/">
           {user ? <Redirect to="/dashboard" /> : <Redirect to="/login" />}
         </Route>
         <Route path="/login" component={Login} />

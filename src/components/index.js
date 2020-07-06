5cc0c857984b65afa0d3272c448f8a176817b4aa
Layla <laylacsa@msn.com>
2020-07-06 12:08:14 -0300

    Deleting imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
 export { default as AlertBadge } from '../dashboard/sections/Alerts/AlertBadge';
 export * from './graphs';
 export * from './layoutPieces';
-export * from '../dashboard/sections/ProcessingTime';
-export * from '../dashboard/sections/YourDesk';

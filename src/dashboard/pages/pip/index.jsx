777a745f1eeb03546550585e86a310c4e37b8ac1
Layla <laylacsa@msn.com>
2020-07-06 15:40:41 -0300

    Rewriting the Alerts component to be functional and use hooks

--- src/dashboard/pages/pip/index.jsx
+++ src/dashboard/pages/pip/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import '../styles.css';
 import './styles.css';
+import '../styles.css';
 
 import {
   Today,
@@ -19,13 +19,13 @@ const propTypes = { userName: PropTypes.string.isRequired };
 function Pip({ userName, user }) {
   return (
     <div className="base-grid pip-grid">
-      {/*<Today user={user} userName={userName} />*/}
+      {/* <Today user={user} userName={userName} /> */}
       <YourDesk user={userName} />
-      {/*<Alerts user={userName} />*/}
+      <Alerts />
       <PerformanceRadar.Pip user={userName} />
       <MainInvestigated user={userName} />
       <SuccessIndicators user={userName} />
-      {/*<ProcessingTime user={user} />*/}
+      {/* <ProcessingTime user={user} /> */}
     </div>
   );
 }
--- src/dashboard/sections/Alerts/index.js
+++ src/dashboard/sections/Alerts/index.js
@@ -1,82 +1,164 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './styles.css';
+import { useAuth } from '../../../app/authContext';
+
 import Api from '../../../api';
 import { getUser } from '../../../user';
 import AlertBadge from './AlertBadge';
 import { SectionTitle, Spinner } from '../../../components';
 import { cleanAlertList } from './alertFormatters';
 
-class AlertsOld extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
-  }
+// class AlertsOld extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { loading: true };
+//   }
+//
+//   componentDidMount() {
+//     this.loadComponent();
+//   }
+//
+//   async loadComponent() {
+//     const [alertList, errorAlerts] = await this.loadAlerts();
+//     const [alertsCount, errorAlertsCount] = await this.loadAlertCount();
+//
+//     const alerts = cleanAlertList(alertList, alertsCount);
+//     const error = errorAlerts || errorAlertsCount;
+//
+//     this.setState({ alerts, loading: false, error });
+//   }
+//
+//   /**
+//    * Fetches array of alerts from API and saves to state
+//    * @return {void} saves to state
+//    */
+//   async loadAlerts() {
+//     let alerts;
+//     let errorAlerts = false;
+//     try {
+//       alerts = await Api.getAlerts(getUser());
+//     } catch (e) {
+//       errorAlerts = true;
+//     }
+//     return [alerts, errorAlerts];
+//   }
+//
+//   async loadAlertCount() {
+//     let alertsTotal;
+//     let errorAlertsTotal = false;
+//     try {
+//       alertsTotal = await Api.getAlertsCount(getUser());
+//     } catch (e) {
+//       errorAlertsTotal = true;
+//     }
+//     return [alertsTotal, errorAlertsTotal];
+//   }
+//
+//   render() {
+//     const { alerts, error, loading } = this.state;
+//     // const loading = !(alerts || errorAlerts) || !(alertsTotal || errorAlertsTotal);
+//
+//     if (loading) {
+//       return (
+//         <article className="alerts-wrapper">
+//           <Spinner size="large" />
+//         </article>
+//       );
+//     }
+//
+//     if (error) {
+//       return <article className="alerts-wrapper">Não existem alertas para exibir.</article>;
+//     }
+//
+//     return (
+//       <article className="alerts-wrapper">
+//         <div className="alerts-header">
+//           <SectionTitle value="central de alertas" />
+//           <div className="alerts-total">{/* <span>{alertsTotal.length}</span> */}</div>
+//         </div>
+//         <div className="alerts-body">
+//           {alerts.map((alert, i) => {
+//             const { icon, message, action, actionLink, background } = alert;
+//             return (
+//               <AlertBadge
+//                 key={alert.docNum + alert.date + i}
+//                 icon={icon}
+//                 iconBg={background}
+//                 message={message}
+//                 action={action}
+//                 actionLink={actionLink}
+//               />
+//             );
+//           })}
+//         </div>
+//       </article>
+//     );
+//   }
+// }
 
-  componentDidMount() {
-    this.loadComponent();
-  }
+function Alerts() {
+  const { user } = useAuth();
+  const [alerts, setAlerts] = useState(undefined);
+  const [alertsError, setAlertsError] = useState(false);
+  const loading = !alerts && !alertsError;
 
-  async loadComponent() {
-    const [alertList, errorAlerts] = await this.loadAlerts();
-    const [alertsCount, errorAlertsCount] = await this.loadAlertCount();
-
-    const alerts = cleanAlertList(alertList, alertsCount);
-    const error = errorAlerts || errorAlertsCount;
-
-    this.setState({ alerts, loading: false, error });
-  }
-
-  /**
-   * Fetches array of alerts from API and saves to state
-   * @return {void} saves to state
-   */
-  async loadAlerts() {
-    let alerts;
-    let errorAlerts = false;
+  async function loadAlerts() {
+    let alertList;
+    let listError = false;
     try {
-      alerts = await Api.getAlerts(getUser());
+      alertList = await Api.getAlerts(user);
     } catch (e) {
-      errorAlerts = true;
+      listError = true;
     }
-    return [alerts, errorAlerts];
+    return [alertList, listError];
   }
 
-  async loadAlertCount() {
+  async function loadAlertCount() {
     let alertsTotal;
     let errorAlertsTotal = false;
     try {
-      alertsTotal = await Api.getAlertsCount(getUser());
+      alertsTotal = await Api.getAlertsCount(user);
     } catch (e) {
       errorAlertsTotal = true;
     }
     return [alertsTotal, errorAlertsTotal];
   }
 
-  render() {
-    const { alerts, error, loading } = this.state;
-    // const loading = !(alerts || errorAlerts) || !(alertsTotal || errorAlertsTotal);
+  async function loadComponent() {
+    const [alertList, errorAlerts] = await loadAlerts();
+    const [alertsCount, errorAlertsCount] = await loadAlertCount();
 
-    if (loading) {
-      return (
-        <article className="alerts-wrapper">
-          <Spinner size="large" />
-        </article>
-      );
-    }
+    const cleanList = cleanAlertList(alertList, alertsCount);
+    const apiError = errorAlerts || errorAlertsCount;
 
-    if (error) {
-      return <article className="alerts-wrapper">Não existem alertas para exibir.</article>;
-    }
+    setAlerts(cleanList);
+    setAlertsError(apiError);
+  }
 
+  // runs on "mount" only
+  useEffect(loadComponent, []);
+
+  if (loading) {
     return (
       <article className="alerts-wrapper">
-        <div className="alerts-header">
-          <SectionTitle value="central de alertas" />
-          <div className="alerts-total">{/* <span>{alertsTotal.length}</span> */}</div>
+        <Spinner size="large" />
+      </article>
+    );
+  }
+
+  return (
+    <article className="alerts-wrapper">
+      <div className="alerts-header">
+        <SectionTitle value="central de alertas" glueToTop />
+        <div className="alerts-total">
+          <span>{alertsError ? 0 : alerts.length}</span>
         </div>
-        <div className="alerts-body">
-          {alerts.map((alert, i) => {
+      </div>
+      <div className="alerts-body">
+        {alertsError && 'Não existem alertas para exibir.'}
+        {alerts &&
+          alerts.map((alert, i) => {
             const { icon, message, action, actionLink, background } = alert;
             return (
               <AlertBadge
@@ -89,21 +171,9 @@ class AlertsOld extends React.Component {
               />
             );
           })}
-        </div>
-      </article>
-    );
-  }
-}
-
-function Alerts() {
-  return (
-    <article className="alerts-wrapper">
-      <div className="alerts-header">
-        <SectionTitle value="central de alertas" />
-        <div className="alerts-total">{/* <span>{alertsTotal.length}</span> */}</div>
       </div>
     </article>
-  )
+  );
 }
 
 export default Alerts;

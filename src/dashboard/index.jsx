e038d3354dfb2de2db5534d4b0f9658762ba1a04
re-andrade1987 <renataandrade1987@hotmail.com>
2020-07-06 11:57:27 -0300

    Ajusting files

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
 import { useAuth } from '../app/authContext';
-// import { Spinner } from '../components/layoutPieces';
+//import { Spinner } from '../components/layoutPieces';
 import { Pip, Tutela, BlankPage } from './pages';
 
-import './styles.css';
 
 function Dashboard() {
+  //const [loading, setLoading] = useState(true);
+
   const { user } = useAuth();
   const { tipoOrgao } = user;
 
@@ -19,33 +20,10 @@ function Dashboard() {
       return <BlankPage />;
   }
 
-  // const [loading, setLoading] = useState(true);
-
-  // function pageSelector() {
-  //   // const { user, userError } = authStore;
-  //   // const loading = !(user || userError);
-  //   let page = <BlankPage />;
-  //
-  //   if (!loginError) {
-  //     switch (tipo_orgao) {
-  //       case 1:
-  //         page = <Tutela userName={nome} user={user} />;
-  //         break;
-  //       case 2:
-  //         page = <Pip userName={nome} user={user} />;
-  //         break;
-  //       default:
-  //         // if we don't have a dashboard yet, just show blank screen
-  //         break;
-  //     }
-  //   }
-  //
-  //   return page;
-  // }
 
   // if (loading) {
-  //   return <Spinner size="large" />;
-  // }
+  //return <Spinner size="large" />;
+  //}
 }
 
 export default Dashboard;

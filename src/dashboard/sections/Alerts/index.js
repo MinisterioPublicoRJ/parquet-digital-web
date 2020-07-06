dd1d1d317eaccdef4f25f5ea587195ca23e75abe
Layla <laylacsa@msn.com>
2020-07-06 14:38:41 -0300

    Changing how svgs are imported

--- src/dashboard/sections/Alerts/index.js
+++ src/dashboard/sections/Alerts/index.js
@@ -3,20 +3,9 @@ import React from 'react';
 import './styles.css';
 import Api from '../../../api';
 import { getUser } from '../../../user';
-import AlertBadge  from './AlertBadge';
+import AlertBadge from './AlertBadge';
 import SectionTitle from '../../../components/layoutPieces/SectionTitle';
-
-
-import ClockIcon from '../../../assets/svg/clock';
-import CorujaGate from '../../../assets/svg/corujaGate';
-import Home from '../../../assets/svg/home';
-import Ouvidoria from '../../../assets/svg/ouvidoria';
-import Va from '../../../assets/svg/va';
-import Tjrj from '../../../assets/svg/tjrj';
-import Law from '../../../assets/svg/law';
-import Mprj from '../../../assets/svg/mprj';
-import Csi from '../../../assets/svg/csi';
-
+import { ClockIcon, CorujaGate, Home, Ouvidoria, Va, Tjrj, Law, Mprj, Csi } from '../../../assets';
 
 class Alerts extends React.Component {
   constructor(props) {
@@ -65,7 +54,8 @@ class Alerts extends React.Component {
         icon = <Tjrj />;
         message = (
           <span>
-            O <strong> processo criminal {` ${` ${alert.docNum} `} `}</strong> está no TJRJ há <strong>mais de 60 dias</strong> sem retorno.
+            O <strong> processo criminal {` ${` ${alert.docNum} `} `}</strong> está no TJRJ há{' '}
+            <strong>mais de 60 dias</strong> sem retorno.
           </span>
         );
         background = '#F86C72';
@@ -75,7 +65,8 @@ class Alerts extends React.Component {
         icon = <Tjrj />;
         message = (
           <span>
-            O <strong>processo não criminal {` ${alert.docNum} `}</strong> está no TJRJ há <strong>mais de 120 dias</strong> sem retorno.
+            O <strong>processo não criminal {` ${alert.docNum} `}</strong> está no TJRJ há{' '}
+            <strong>mais de 120 dias</strong> sem retorno.
           </span>
         );
         background = '#F86C72';
@@ -85,7 +76,10 @@ class Alerts extends React.Component {
         icon = <Home />;
         message = (
           <span>
-            O <strong>orgão responsável</strong> pelo processo <strong>{` ${alert.docNum} `}</strong> está possivelmente <strong> desatualizado</strong>.
+            O <strong>orgão responsável</strong> pelo processo{' '}
+            <strong>{` ${alert.docNum} `}</strong> está possivelmente
+{' '}
+            <strong> desatualizado</strong>.
           </span>
         );
         background = '#5C6FD9';
@@ -95,7 +89,8 @@ class Alerts extends React.Component {
         icon = <Ouvidoria />;
         message = (
           <span>
-            O processo <strong> {` ${alert.docNum} `}</strong> possui <strong>vitimas recorrentes</strong> de violência domestica.
+            O processo <strong> {` ${alert.docNum} `}</strong> possui{' '}
+            <strong>vitimas recorrentes</strong> de violência domestica.
           </span>
         );
         background = '#F86C72';
@@ -105,7 +100,8 @@ class Alerts extends React.Component {
         icon = <ClockIcon />;
         message = (
           <span>
-            O<strong> processo administrativo {` ${alert.docNum} `} </strong> está aberto <strong>há mais de 1 ano</strong>.
+            O<strong> processo administrativo {` ${alert.docNum} `} </strong> está aberto{' '}
+            <strong>há mais de 1 ano</strong>.
           </span>
         );
         background = '#5C6FD9';
@@ -115,8 +111,12 @@ class Alerts extends React.Component {
         icon = <ClockIcon />;
         message = (
           <span>
-            O <strong>procedimento preparatório {` ${alert.docNum} `}</strong> está com prazo próximo de vencer.
-          </span>
+            O <strong>
+procedimento preparatório{` ${alert.docNum} `}</strong>
+{' '}
+está com prazo próximo
+            de vencer.
+</span>
         );
         background = '#f86c72';
         break;
@@ -125,7 +125,13 @@ class Alerts extends React.Component {
         icon = <ClockIcon />;
         message = (
           <span>
-            O inquérito civil ativo <strong> {` ${alert.docNum} `} </strong> <strong> está sem prorrogação </strong> há <strong>mais de 1 ano</strong>.
+            O inquérito civil ativo <strong>
+{' '}
+{` ${alert.docNum} `}
+{' '}
+ </strong>
+{' '}
+            <strong> está sem prorrogação </strong> há<strong>mais de 1 ano</strong>.
           </span>
         );
         background = '#f86c72';
@@ -135,7 +141,9 @@ class Alerts extends React.Component {
         icon = <ClockIcon />;
         message = (
           <span>
-            A <strong> noticia de fato{` ${alert.docNum} `}</strong> foi autuada há mais de <strong>120 dias</strong> e ainda está <strong>sem tratamento</strong>
+            A <strong> noticia de fato{` ${alert.docNum} `}</strong> foi autuada há mais de{' '}
+            <strong>120 dias</strong> e ainda está
+            <strong>sem tratamento</strong>
           </span>
         );
         background = '#f86c72';
@@ -145,7 +153,8 @@ class Alerts extends React.Component {
         icon = <ClockIcon />;
         message = (
           <span>
-            O ofício <strong>{` ${alert.docNum} `}</strong> está com o <strong> prazo de apreciação esgotado </strong>.
+            O ofício <strong>{` ${alert.docNum} `}</strong> está com o{' '}
+            <strong> prazo de apreciação esgotado </strong>.
           </span>
         );
         background = '#f86c72';
@@ -155,7 +164,8 @@ class Alerts extends React.Component {
         icon = <Ouvidoria />;
         message = (
           <span>
-            O<strong> expediente de ouvidoria {` ${alert.docNum} `}</strong> foi enviado porém <strong> não recebido</strong>
+            O<strong> expediente de ouvidoria {` ${alert.docNum} `}</strong> foi enviado porém{' '}
+            <strong> não recebido</strong>
           </span>
         );
         background = '#5C6FD9';
@@ -165,28 +175,34 @@ class Alerts extends React.Component {
         icon = <Va />;
         message = (
           <span>
-            Você tem uma <strong> vista aberta</strong> no <strong>documento {` ${alert.docNum} `}, sinalizado como fechado</strong>
+            Você tem uma <strong> vista aberta</strong> no{' '}
+            <strong>
+documento{` ${alert.docNum} `}
+, sinalizado como fechado
+</strong>
           </span>
         );
         background = '#28A7E0';
         break;
 
-        case 'DORD':
-          icon = <Mprj />;
-          message = (
-            <span>
-              O processo <strong> {` ${alert.docNum} `}</strong> está possivelmente <strong> desatualizado</strong>.
-            </span>
-          );
-          background = '#5C6FD9';
-          break;
+      case 'DORD':
+        icon = <Mprj />;
+        message = (
+          <span>
+            O processo <strong> {` ${alert.docNum} `}</strong> está possivelmente{' '}
+            <strong> desatualizado</strong>.
+          </span>
+        );
+        background = '#5C6FD9';
+        break;
 
       // ALERTAS DA PIP
       case 'GATE':
         icon = <CorujaGate />;
         message = (
           <span>
-            O <strong>Gate </strong>finalizou a <strong>IT</strong> solicitada no procedimento <strong>{` ${alert.docNum} `}</strong>
+            O <strong>Gate </strong>finalizou a <strong>IT</strong> solicitada no procedimento{' '}
+            <strong>{` ${alert.docNum} `}</strong>
           </span>
         );
         background = '#374354';
@@ -247,7 +263,11 @@ class Alerts extends React.Component {
         icon = <Mprj />;
         message = (
           <span>
-            O <strong>processo {` ${alert.docNum} `}</strong> está possivelmente<strong> desatualizado</strong>
+            O <strong>
+processo{` ${alert.docNum} `}</strong>
+{' '}
+está possivelmente
+<strong> desatualizado</strong>
           </span>
         );
         background = '#5C6FD9';

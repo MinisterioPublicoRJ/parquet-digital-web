1b87378c764084eb350b4d9cad8730bfb02b61bb
Layla <laylacsa@msn.com>
2020-07-08 11:05:32 -0300

    Adding the component back to tutela

--- src/dashboard/pages/tutela/index.jsx
+++ src/dashboard/pages/tutela/index.jsx
@@ -18,12 +18,12 @@ const propTypes = { userName: PropTypes.string.isRequired };
 function Tutela({ userName, user }) {
   return (
     <div className="base-grid tutela-grid">
-      <Alerts />
-      <ProcessList user={userName} />
-      <PerformanceRadar.Tutela user={userName} />
-      {/*<ProcessingTime user={user} />*/}
       {/*<Today userName={userName} user={user} />*/}
       <YourDesk user={userName} />
+      <PerformanceRadar.Tutela user={userName} />
+      <Alerts />
+      <ProcessList user={userName} />
+      <ProcessingTime user={user} />
     </div>
   );
 }

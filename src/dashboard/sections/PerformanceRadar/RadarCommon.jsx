4172b349228622d00dd80ea643685997c72fb61c
Layla <laylacsa@msn.com>
2020-07-09 20:09:31 -0300

    Linting, removing unecessary setStates and adding error state

--- src/dashboard/sections/PerformanceRadar/RadarCommon.jsx
+++ src/dashboard/sections/PerformanceRadar/RadarCommon.jsx
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import { useAuth } from '../../../app/authContext';
 import './styles.css';
 import { Spinner, SectionTitle } from '../../../components/layoutPieces';
-import  PerformanceChart from '../../../components/graphs/PerformanceChart';
-
+import PerformanceChart from '../../../components/graphs/PerformanceChart';
 
 const propTypes = {
   getRadarData: PropTypes.func.isRequired,
@@ -12,60 +11,63 @@ const propTypes = {
   axisLabelsTable: PropTypes.shape.isRequired,
 };
 
-function PerformanceRadar ({ getRadarData, axisLabelsTable, cleanMap }) {
+function PerformanceRadar({ getRadarData, axisLabelsTable, cleanMap }) {
   const { user } = useAuth();
   const [chartData, setChartData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [dataError, setError] = useState(false);
 
   useEffect(() => {
     getPerformanceData();
-  }, [])
+  }, []);
 
   const getPerformanceData = async () => {
-    let res = []
-    setLoading(true);
+    let res = [];
     try {
-    res = await getRadarData(user);
-    setChartData(res);
-    cleanGraphData(res);
+      res = await getRadarData(user);
+      cleanGraphData(res);
     } catch (e) {
-    setLoading(true);
+      setError(true);
     } finally {
-    setLoading(false);
+      setLoading(false);
     }
-  }
+  };
 
-  const cleanGraphData = (data) => {
-    const chartData = Object.entries(data)
+  const cleanGraphData = data => {
+    const cleanData = Object.entries(data)
       .filter(cat => cat[0] !== 'meta')
       .map(cleanMap);
 
-    setChartData(chartData);
-  }
-    if (loading) {
-      return (
-        <article className="page-radar-dashboard">
-          <Spinner size="large" />
-        </article>
-      );
-    }
-  
+    setChartData(cleanData);
+  };
+
+  if (loading || dataError) {
     return (
       <article className="page-radar-dashboard">
         <div className="radar-header">
           <SectionTitle value="Radar de Performance" subtitle="(últimos 180 dias)" glueToTop />
         </div>
-        <figure className="radar-wrapper">
-          <div className="radar-graph">
-            <PerformanceChart axisLabelsTable={axisLabelsTable} data={chartData} />
-          </div>
-          <figcaption className="radar-subtitles">
-            <div className="radar-subtitles-item radar-subtitles-item-yourData">Sua Promotoria</div>
-            <div className="radar-subtitles-item radar-subtitles-item-MPData">Perfil do MP</div>
-          </figcaption>
-        </figure>
+        {loading ? <Spinner size="large" /> : 'Sem dados para exibir'}
       </article>
     );
+  }
+
+  return (
+    <article className="page-radar-dashboard">
+      <div className="radar-header">
+        <SectionTitle value="Radar de Performance" subtitle="(últimos 180 dias)" glueToTop />
+      </div>
+      <figure className="radar-wrapper">
+        <div className="radar-graph">
+          <PerformanceChart axisLabelsTable={axisLabelsTable} data={chartData} />
+        </div>
+        <figcaption className="radar-subtitles">
+          <div className="radar-subtitles-item radar-subtitles-item-yourData">Sua Promotoria</div>
+          <div className="radar-subtitles-item radar-subtitles-item-MPData">Perfil do MP</div>
+        </figcaption>
+      </figure>
+    </article>
+  );
 }
 
 PerformanceRadar.propTypes = propTypes;

a27ff1e62420d566095d2dae0e949ff29fabe149
re-andrade1987 <renataandrade1987@hotmail.com>
2020-07-13 13:40:51 -0300

    adjusting function to show the prosecutor

--- src/dashboard/sections/Today/index.jsx
+++ src/dashboard/sections/Today/index.jsx
@@ -18,6 +18,8 @@ function Today () {
   const { user } = useAuth();
   const [todayPercent, setTodayPercent] = useState([]);
   const [phrase, setPhrase] = useState([]);
+  const [groupName, setgroupName] = useState([]);
+  const [entriesGroup, setEntriesGroup] = useState([]);
   const [dataError, setError] = useState(false);
   const [loading, setLoading] = useState(true);
 
@@ -25,15 +27,17 @@ function Today () {
     loadPercentages();
     loadCollection();
     loadEntriesInfo();
-
   }, []);
 
    /**
    * checks if all the info was fetched from aPI, warns parent when done
    * @param  {boolean} dataError
-   * @return {boolean}
    */
 
+  /**
+   * laods percentage data for the first sentence
+   * @return {void}
+   */
   const loadPercentages = async () => {
     let percentile;
     let res = []
@@ -48,6 +52,10 @@ function Today () {
       setLoading(false);
     }
   };
+   /**
+   * loads/reloads info an calls formatters for second sentence data
+   * @return {void}
+   */
 
   const loadCollection = async () => {
     let collectionPhrase;
@@ -55,31 +63,41 @@ function Today () {
     try {
       const today = new Date();
       const { primQ, terQ, acervoQtd, cod } = await Api.getTodayOutliersData(user, today);
-      //collectionPhrase = analyzeCollection(primQ, terQ, acervoQtd);
+      collectionPhrase = analyzeCollection(primQ, terQ, acervoQtd);
       groupName = NOMES_PROMOTORIAS[cod];
-      console.log(primQ, terQ, acervoQtd, cod)
-      console.log(groupName)
-      setPhrase(primQ, terQ, acervoQtd, cod);
+      setPhrase(collectionPhrase);
+      setgroupName(groupName);
     } catch (e) {
       setError(true);
     } finally {
       setLoading(false);
     }
   }
-   
+   /**
+   * loads/reloads info an calls formatters for third sentence data
+   * @return {void}
+   */
   const loadEntriesInfo = async () => {
     let entriesParagraph;
     try {
       const { hout, lout, numEntries } = await Api.getTodayEntriesData(user);
-      console.log(hout, lout, numEntries)
+      entriesParagraph = analyzeEntries(hout, lout, numEntries);
+      setEntriesGroup(entriesParagraph);
     } catch (e) {
       setError(true);
     } finally {
       setLoading(false);
-      return [entriesParagraph];
     }
   }
-  
+
+/**
+   * compares the number of entries to the business rules to decide which phrase to show. A day can be typical, atypical or empty
+   * @param  {Number} hout   how many entries are on the upper boundary of a typical day
+   * @param  {Number} lout   how many entries are on the lower boundary of a typical day
+   * @param  {Number} amount amount of entries on given day
+   * @return {Node}        React element to be inserted on View
+   */
+
   function analyzeEntries(hout, lout, amount) {
     if (!amount) {
       return (
@@ -102,7 +120,55 @@ function Today () {
       </p>
     );
   }
- 
+   /**
+   * returns the right comment based on collection size
+   * @param  {number} lower  1st quartile limit
+   * @param  {number} higher 3rd quartile limit
+   * @param  {number} amount current collection
+   * @return {string}        sentence to be used in second paragraph
+   */
+  function analyzeCollection(lower, higher, amount) {
+    if (amount < lower) {
+      return 'razoavelmente menor que os';
+    }
+
+    if (amount > higher) {
+      return 'razoavelmente maior que os';
+    }
+
+    return 'de volume regular comparado aos';
+  }
+   /**
+   * Returns the greeting to be shown on the page
+   * @return {string} [description]
+   */
+  {/*function assembleGreeting() {
+    const user = cleanUsername();
+    const hours = new Date().getHours();
+    const gender = user.sexo;
+    
+    let timeGreeting;
+
+    if (hours >= 6 && hours < 12) {
+      timeGreeting = 'bom dia';
+    } else if (hours >= 12 && hours < 18) {
+      timeGreeting = 'boa tarde';
+    } else {
+      timeGreeting = 'boa noite';
+    }
+
+    return `Olá ${gender === 'M' ? 'Dr. ' : 'Dra.'} ${user}, ${timeGreeting}!`;
+  }*/}
+   /**
+   * Gets the original string returned from the API, trims and prettifies it.
+   * @return {string} First and last names, just the first letter of each capitalized
+   */
+  {/*function cleanUsername() {
+    const { userName } = user;
+    const cleanUsername = userName.split('')[0];
+    return capitalizeTitle(cleanUsername);
+  }*/}
+
     if (loading || dataError) {
       return (
         <article className="today-outer">
@@ -113,7 +179,26 @@ function Today () {
         </article>
       );
     }
-   
+    //const greeting = assembleGreeting();
+
+    const percentParagraph = !todayPercent ? null : (
+      <p className="today-textArea-paragraphWrapper">
+        No último mês a sua promotoria foi mais resolutiva que
+        <span style={{ fontWeight: 'bold' }}>{` ${todayPercent} `}</span>
+        da casa entre aquelas de mesma atribuição.
+        {todayPercent > 0.5 && <span style={{ fontWeight: 'bold' }}>Parabéns!</span>}
+      </p>
+    );
+    const collectionParagraph = !phrase ? null : (
+      <p className="today-textArea-paragraphWrapper">
+        Você sabia que seu acervo é
+        <span style={{ fontWeight: 'bold' }}>{` ${phrase} `}</span>
+        dos seus colegas das
+        <span style={{ fontWeight: 'bold' }}>{` ${groupName}`}</span>
+        ?
+      </p>
+    );
+
     return (
       <article className="today-outer">
         <div className="today-header">
@@ -122,15 +207,10 @@ function Today () {
           <div className="today-content">
             <SectionTitle value="resumo do dia" glueToTop />
             <div className="today-textArea">
-            <p className="today-textArea-paragraphWrapper">
-            No último mês a sua promotoria foi mais resolutiva que
-            <span style={{ fontWeight: 'bold' }}>{` ${todayPercent} `}</span>
-            da casa entre aquelas de mesma atribuição.
-            {todayPercent > 0.5 && <span style={{ fontWeight: 'bold' }}>Parabéns!</span>}
-            </p>
-           {/*collectionParagraph}
-            {entriesParagraph*/}
-          </div>
+              {percentParagraph}
+              {collectionParagraph}
+              {entriesGroup}
+            </div>
         </div>
         <button type="button" className="today-btn">
           Ver mapa da atuação

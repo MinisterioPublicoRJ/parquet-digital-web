da16fbeeccd2257173ab415b7f45ec9401ba70de
re-andrade1987 <renataandrade1987@hotmail.com>
2020-07-15 21:32:01 -0300

    Crating function to send post and logar

--- src/api/transforms/userTransform.js
+++ src/api/transforms/userTransform.js
@@ -1,10 +1,5 @@
-import { snakeToCamel } from '../../utils';
+//import { snakeToCamel } from '../../utils';
 
-export default function userTransform(userObj) {
-  const res = {};
-  const keys = Object.keys(userObj);
-  keys.forEach(key => {
-    res[snakeToCamel(key)] = userObj[key];
-  });
-  return res;
+export default function loginTransform(data) {
+  return data;
 }
--- src/login/index.jsx
+++ src/login/index.jsx
@@ -1,58 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
 import './styles.css';
-//import { useAuth } from '../../../app/authContext';
-
+import { useAuth } from '../app/authContext';
 import Api from '../api';
 import { LoginPromotron } from '../assets';
 
 const Login = () => {
-  const [login, setLogin] = useState([{ name: '', email: '' }]);
+  const { user } = useAuth();
+  const [login, setLogin] = useState([{ matricula: '', senha: '' }]);
+  const [loginError, setloginError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleChange = event => {
-    setLogin({ ...login, [event.target.name]: event.target.value });
-    console.log(login);
+    setLogin({ ...login, [event.target.matricula]: event.target.value });
   };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  {/*if (loading) {
+    return <h1>Carregando!!!</h1>;
+  }*/}
 
   return (
     <div className="wrapper">
       <div className="wrapper-login">
-        <div className="Login-intro-container">
-          {/*
-          * @Todo substituir img de background e colocar o texto com formatação adequada
-          *
-          <h1>PARQUET DIGITAL</h1>
-          <p>
-            Painel multitarefas criado para auxiliar a gestão, o entendimento do dia-a-dia com base
-            em evidências e uma análise apurada da sua Promotoria.
-          </p>
-          <img
-            height="100%"
-            width="100%"
-            src={require('../assets/imgs/mprj-em-mapas.png')}
-            alt="MPRJ Em Mapas"
-          />
-          */}
-        </div>
+        <div className="Login-intro-container" />
         <div className="Login-container">
           <div className="Login-banner">
-            {/*
-            * GIF
-            * <img
-            height="100%"
-            width="100%"
-            src={require('../assets/gifs/promotron.gif')}
-            alt="robô-promoton"
-          /> */}
             <LoginPromotron />
           </div>
-          <form className="Login-form">
+          <form className="Login-form" onSubmit={handleSubmit}>
             <div className="Login-inputs">
               <input
                 className="Login-input"
                 placeholder="Matrícula MPRJ"
                 type="text"
-                value={login.name}
+                value={login.matricula}
                 onChange={handleChange}
                 required
               />
@@ -60,7 +45,7 @@ const Login = () => {
                 className="Login-input"
                 placeholder="Senha"
                 type="password"
-                value={login.email}
+                value={login.senha}
                 onChange={handleChange}
                 required
               />

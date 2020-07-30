33012e7fb0507218fa5eb6f3870a9de931295067
re-andrade1987 <renataandrade1987@hotmail.com>
2020-07-30 12:45:34 -0300

    Creating mock to show prhases inside modal

---
+++ src/dashboard/sections/introduction/index.js
@@ -0,0 +1,43 @@
+export const INTRODUCTION = [
+  {
+    title: 'Resumo do dia',
+    subtitle:'Aqui você verá as atualizações do dia na sua Promotoria, trazemos várias informações baseado em uma rotina de atualização do robô. Ele busca o desempenho do trabalho realizado nos últimos 30 dias bem como verifica o volume de entrada de processos e traz uma classificação baseado nesta frequência.)',
+    definition: 'texto explicativo',
+  },
+  {
+    title: 'Sua mesa',
+    subtitle: 'Espaço do painel que você encontra todo o movimento dos processos da sua Promotoria. Separados em Vistas Abertas, PIC, Inquéritos, AISP e sinalizamos também a quantidade de finalizados nos últimos 30 dias. Logo abaixo é possível ainda filtrar as vistas pela vida do processo na sua mesa. Por último a lista dos processos propriamente dita.',
+    definition: 'texto explicativo',
+  },
+  {
+    title: 'Radar de performance',
+    subtitle: 'Espaço do painel que você encontra todo o movimento dos processos da sua Promotoria. Separados em Vistas Abertas, PIC, Inquéritos, AISP e sinalizamos também a quantidade de finalizados nos últimos 30 dias. Logo abaixo é possível ainda filtrar as vistas pela vida do processo na sua mesa. Por último a lista dos processos propriamente dita.',
+    definition: 'texto explicativo',
+  },
+  {
+    title: 'Principais investigados',
+    subtitle: 'Espaço do painel que você encontra todo o movimento dos processos da sua Promotoria. Separados em Vistas Abertas, PIC, Inquéritos, AISP e sinalizamos também a quantidade de finalizados nos últimos 30 dias. Logo abaixo é possível ainda filtrar as vistas pela vida do processo na sua mesa. Por último a lista dos processos propriamente dita.',
+    definition: 'texto explicativo',
+  },
+  {
+    title: 'Lista de processos',
+    subtitle: 'Espaço do painel que você encontra todo o movimento dos processos da sua Promotoria. Separados em Vistas Abertas, PIC, Inquéritos, AISP e sinalizamos também a quantidade de finalizados nos últimos 30 dias. Logo abaixo é possível ainda filtrar as vistas pela vida do processo na sua mesa. Por último a lista dos processos propriamente dita.',
+    definition: 'texto explicativo',
+  },
+  {
+    title: 'Central de alertas',
+    subtitle: 'Espaço do painel que você encontra todo o movimento dos processos da sua Promotoria. Separados em Vistas Abertas, PIC, Inquéritos, AISP e sinalizamos também a quantidade de finalizados nos últimos 30 dias. Logo abaixo é possível ainda filtrar as vistas pela vida do processo na sua mesa. Por último a lista dos processos propriamente dita.',
+    definition: 'texto explicativo',
+  },
+  {
+    title: 'Indicadores de sucesso',
+    subtitle: 'Espaço do painel que você encontra todo o movimento dos processos da sua Promotoria. Separados em Vistas Abertas, PIC, Inquéritos, AISP e sinalizamos também a quantidade de finalizados nos últimos 30 dias. Logo abaixo é possível ainda filtrar as vistas pela vida do processo na sua mesa. Por último a lista dos processos propriamente dita.',
+    definition: 'texto explicativo',
+  },
+  {
+    title: 'Tempo de tramitação',
+    subtitle: 'Espaço do painel que você encontra todo o movimento dos processos da sua Promotoria. Separados em Vistas Abertas, PIC, Inquéritos, AISP e sinalizamos também a quantidade de finalizados nos últimos 30 dias. Logo abaixo é possível ainda filtrar as vistas pela vida do processo na sua mesa. Por último a lista dos processos propriamente dita.',
+    definition: 'texto explicativo',
+  },
+  
+];

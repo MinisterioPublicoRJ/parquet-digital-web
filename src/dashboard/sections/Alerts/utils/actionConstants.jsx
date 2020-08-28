0133f5227d84ace7c38dc51f564ebc11229a5853
Layla <laylacsa@msn.com>
2020-08-28 17:50:32 -0300

    Adding some new actions

--- src/dashboard/sections/Alerts/utils/actionConstants.jsx
+++ src/dashboard/sections/Alerts/utils/actionConstants.jsx
@@ -8,16 +8,34 @@ export const DELETE = {
   text: 'dispensar',
   background: 'transparent',
 };
-export const TESTE = {
-  actionType: 'teste',
+
+export const COMPRAS = link => ({
+  actionType: 'link',
+  icon: <IconCompras fillColor="white" width="30px" height="30px" />,
+  text: 'Painel de Compras',
+  background: '#F8BD6C',
+  link,
+});
+
+export const OUVIDORIA = link => ({
+  actionType: 'link',
   icon: <Ouvidoria fillColor="white" width="30px" height="30px" />,
-  text: 'teste! painel de compras',
-  background: 'orange',
-};
+  text: 'Ouvidoria',
+  background: '#5C6FD9',
+  link,
+});
+
 export const IT = link => ({
   actionType: 'download',
   icon: <CorujaGate fillColor="white" width="30px" height="30px" />,
-  text: 'Download da IT',
-  background: '#374354',
+  text: 'Baixar IT',
+  background: '#71D0A4',
   link,
+});
+
+export const CALCULO = () => ({
+  actionType: 'overlay',
+  icon: <CorujaGate fillColor="white" width="30px" height="30px" />,
+  text: 'Ver Cálculo',
+  background: '#F8BD6C',
 });

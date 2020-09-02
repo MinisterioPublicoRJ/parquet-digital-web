2637fd2a6f91142f3f92de696965bd84d21287db
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-09-02 16:52:07 -0300

    Changes title for inquiries' detail 1st ranking

--- src/dashboard/sections/YourDesk/Ranking/titleConstants.js
+++ src/dashboard/sections/YourDesk/Ranking/titleConstants.js
@@ -1,7 +1,7 @@
 const TITLEDICT = {
   variacao_documentos_distintos: 'maiores reduções de acervo do mês',
   acervo_fim: 'maiores volumes do mês',
-  nr_documentos_distintos_atual: 'Procedimentos Tratados No Mês',
+  nr_documentos_distintos_atual: 'Documentos com Vista No Mês',
   nr_aproveitamentos_atual: 'maiores aproveitamentos do mês',
   variacao_acervo: 'Maiores Reduções do Mês',
   nr_acoes_ultimos_30_dias: 'Recorde de Ações Ajuizadas no Mês',

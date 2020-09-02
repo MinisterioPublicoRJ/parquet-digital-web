6142632fa48e9d38257800ad030165eaf3067f3f
Layla <laylacsa@msn.com>
2020-09-02 17:05:15 -0300

    REmoving unecessary props

--- src/dashboard/sections/Alerts/AlertBadge/AlertActionButtons/index.jsx
+++ src/dashboard/sections/Alerts/AlertBadge/AlertActionButtons/index.jsx
@@ -4,18 +4,13 @@ import PropTypes from 'prop-types';
 import './styles.css';
 
 const propTypes = {
-  actionType: PropTypes.string.isRequired,
   background: PropTypes.string.isRequired,
   clickCallback: PropTypes.func.isRequired,
   icon: PropTypes.node.isRequired,
-  link: PropTypes.string,
   text: PropTypes.node.isRequired,
 };
-const defaultProps = {
-  link: null,
-};
 
-function AlertActionButtons({ actionType, icon, background, text, link, clickCallback }) {
+function AlertActionButtons({ icon, background, text, clickCallback }) {
   return (
     <button
       className="actionButton-outer"
@@ -30,5 +25,4 @@ function AlertActionButtons({ actionType, icon, background, text, link, clickCal
 }
 
 AlertActionButtons.propTypes = propTypes;
-AlertActionButtons.defaultProps = defaultProps;
 export default AlertActionButtons;

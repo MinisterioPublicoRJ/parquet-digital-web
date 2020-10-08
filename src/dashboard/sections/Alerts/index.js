f9709cfebfd0d3c3162630457494fe821d7aa8a2
majard <mahjard@protonmail.com>
2020-10-08 18:33:45 -0300

    Update index.js
    erases console log

--- src/dashboard/sections/Alerts/index.js
+++ src/dashboard/sections/Alerts/index.js
@@ -23,7 +23,6 @@ function Alerts() {
     let listError = false;
     try {
       alertList = await Api.getAlerts(buildRequestParams());
-      console.log(alertList);
     } catch (e) {
       listError = true;
     }

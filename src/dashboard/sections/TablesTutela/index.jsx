0363654cc15bc371abb03b2651ba87d04d621c95
re-andrade1987 <renataandrade1987@hotmail.com>
2020-10-08 18:53:05 -0300

    ajusting fade in

--- src/dashboard/sections/TablesTutela/index.jsx
+++ src/dashboard/sections/TablesTutela/index.jsx
@@ -7,19 +7,22 @@ import './styles.css';
 const TablesTutela = () => {
   // eslint-disable-next-line no-shadow
   const [showTables, setshowTables] = useState([]);
-  const [fade, setFadein] = useState(false);
+  const [fadeIn, setFadeIn] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
+
   const handleToggle = () => setshowTables(!showTables);
 
   return (
     <div className="processList-outer">
-      <div className="processList-tableWrapper" id="={isFadingOut ? 'item-fadeout' : 'item'}">
-          <button onClick={handleToggle} className="button-tables">
-          { showTables  &&  
-          <OngoingInvestigations className={`text ${showTables ? 'visible' : ''}`} /> }
-          </button>
-          <button onClick={handleToggle} className="button-tables">
-          { !showTables && <ProcessList />}
-          </button>
+      <div className="processList-tableWrapper">
+        <button onClick={handleToggle} className="button-tables">
+          {showTables && (
+            <OngoingInvestigations />
+          )}
+        </button>
+        <button onClick={handleToggle} className="button-tables">
+          {!showTables && <ProcessList />}
+        </button>
       </div>
     </div>
   );

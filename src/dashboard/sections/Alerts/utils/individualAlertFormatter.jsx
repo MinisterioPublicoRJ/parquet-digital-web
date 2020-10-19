1688538f48abf33e29e9ea2693133c16419510ed
Mah <mahjard@protonmail.com>
2020-10-19 17:42:21 -0300

    Adds unsent occurence list donwload button to RO alert

--- src/dashboard/sections/Alerts/utils/individualAlertFormatter.jsx
+++ src/dashboard/sections/Alerts/utils/individualAlertFormatter.jsx
@@ -20,6 +20,7 @@ import {
   IC1A_ACTION_GENERATE_DOC,
   PPFP_ACTION_EXTEND,
   PPFP_ACTION_CONVERT,
+  UNSENT_OCCURRENCE_LIST,
 } from '../../../../api/endpoints';
 
 import {
@@ -33,6 +34,7 @@ import {
   GENERATE_DOC,
   GENERATE_MINUTA,
   EXTEND_DEADLINE,
+  DOWNLOAD_LIST,
 } from './actionConstants';
 
 /**
@@ -85,7 +87,7 @@ export default function individualAlertFormatter(alert, cpf, token, orgao) {
       return compConstructor(alert, orgao, token);
 
     case 'RO':
-      return roOccurrence(alert);
+      return roOccurrence(alert, token);
 
     // ALERTAS DE PRESCRIÇÃO
     case 'PRCR':
@@ -775,8 +777,10 @@ function dt2iConstructor({ dropdown, alertCode, count, docNum }, alert) {
   };
 }
 
-function roOccurrence(alert) {
+function roOccurrence(alert, token) {
   const { dropdown, alertCode, count, daysPassed, alertId } = alert;
+  console.log('token: ', token);
+  const dpNumber = alertId;
   let key;
   let message;
 
@@ -795,19 +799,20 @@ function roOccurrence(alert) {
     message = (
       <span>
         <strong>{` ${daysPassed} ${single ? 'registro' : 'registros'} `}</strong>
-        de ocorrência da <strong>{` ${alertId}`}ª DP</strong> não chegaram no MPRJ
+        de ocorrência da <strong>{` ${dpNumber}`}ª DP</strong> não chegaram no MPRJ
       </span>
     );
   }
 
   return {
-    actions: [DETAIL(), DELETE],
+    actions: [DOWNLOAD_LIST(UNSENT_OCCURRENCE_LIST({ dpNumber, token })), DETAIL(), DELETE],
     backgroundColor: '#F8BD6C',
     icon: <Ro />,
     key,
     message,
   };
 }
+
 function ctacConstructor({ dropdown, alertCode, count, docNum }) {
   let key;
   let message;

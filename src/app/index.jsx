8952ad1994d766ca81bc32290284ba8ce12decee
Layla <laylacsa@msn.com>
2021-03-03 11:33:41 -0300

    BugFix on Today, new background colors for alerts, and finally GA

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './styles.css';
 import '../themes/index.css';
-// import ReactGA from 'react-ga';
 import Router from './router';
 import AuthContext from './authContext';
 import Api from '../api';
@@ -114,9 +113,6 @@ function App() {
   if (!(user || userError)) {
     return <Spinner size="large" />;
   }
-
-  // ReactGA.initialize('UA-80844385');
-  // ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
     <AuthContext.Provider value={authStore}>

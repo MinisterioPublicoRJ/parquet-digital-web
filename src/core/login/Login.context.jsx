79a4aced21b0e23ffc4f8c166494ff5d99bbae32
Layla <laylacsa@msn.com>
2021-07-20 13:41:48 -0300

    Changing tasks

---
+++ src/core/login/Login.context.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const LoginContext = createContext();
+
+export const LoginProvider = ({ store, children }) => (
+  <LoginContext.Provider value={store}>{children}</LoginContext.Provider>
+);
+
+export const useLoginContext = () => useContext(LoginContext);
+
+export function LoginStoreInitializer() {
+  const [username, setUsername] = useState('');
+  const [secret, setSecret] = useState('');
+  const [isLoading, setLoadingState] = useState(false);
+
+  return {
+  };
+}

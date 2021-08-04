c8d5bd1c40cbf9a962641176d7ca7b7541a801be
Ministerio Publico <ministeriopublico@mac-camilla.local>
2021-08-04 14:30:42 -0300

    Add file home page inside welcome and css modules inside file

---
+++ src/web/views/welcomePages/home.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Run from '../../../../assets/gifs/run.gif';
+import {
+    wrapperWelcome,
+    bannerWelcome,
+    welcomeTexts,
+    allAtns,
+    divMainPrevious,
+    btnPrevious,
+    btns,
+    btnNext,
+    btnJumper
+    
+} from './WelcomePages.module.css';
+  
+const Home = () => {
+  return (
+    <div className={wrapperWelcome}>
+      <div className={bannerWelcome}>
+        <img height="100%" src={Run} alt="trabalho-promoton" />
+      </div>
+      <div className={welcomeTexts}>
+        <h2>Celeridade nos Processos</h2>
+        <p>
+          O robô das promotorias tem como missão ajudar o trabalho do Promotor, tornando sua atuação
+          ainda mais célere e capaz de atingir o maior impacto possível.
+        </p>
+      </div>
+      <div className={allAtns}>
+        <div className={divMainPrevious}>
+          <Link to="./work">
+            <button type="button" className={btnPrevious}>
+              anterior
+            </button>
+          </Link>
+        </div>
+        <div className={btns}>
+          <Link to="./perfomanceAnalysis">
+            <button type="button" className={btnNext}>
+              próximo
+            </button>
+          </Link>
+          <Link to="./dashboard">
+            <button type="button" className={btnJumper}>
+              pular
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

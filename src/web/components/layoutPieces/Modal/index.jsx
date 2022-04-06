ff0320cfe6c8c18cc377c22d74b533ede2fbac6d
majard <mahjard@protonmail.com>
2022-04-06 19:00:53 -0300

    Tabtrap functional

--- src/web/components/layoutPieces/Modal/index.jsx
+++ src/web/components/layoutPieces/Modal/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, {useRef, inputRef} from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactDom from 'react-dom';
 import './styles.css';
@@ -16,37 +16,44 @@ const defaultProps = { children: undefined };
  */
 function handleInnerClick(e) {
   e.stopPropagation();
-
 }
 
-const TabTrap = (e) => {
-  if (e.key !== 'Tab') return
+const TabTrap = (e, close) => {
 
-  const focusableModalElements = Modal.current.querySelectorAll(
-    'a[href], button:not([disabled]), textarea, input, select'
-  )
+  if (e.key === 'Escape') {
+    close();
+    return;
+  }
+  const focusableModalElements = document
+    .querySelector('#portal')
+    .querySelectorAll('a[href], button:not([disabled]), textarea, input, select, [role="button"]');
 
-  const firstElement = focusableModalElements[0]
-  const lastElement =
-    focusableModalElements[focusableModalElements.length - 1]
+  const firstElement = focusableModalElements[0];
+  const lastElement = focusableModalElements[focusableModalElements.length - 1];
 
   if (!e.shiftKey && document.activeElement === lastElement) {
+
     firstElement.focus();
-    return e.preventDefault();
+    e.preventDefault();
+    return;
   }
   if (e.shiftKey && document.activeElement === firstElement) {
-    lastElement.focus()
-    e.preventDefault()
+
+    lastElement.focus();
+    e.preventDefault();
   }
-}
+};
 
 export default function Modal({ children, close }) {
-  
+  useEffect(() => {
+    document.querySelector('.modal-outer').focus();
+  }, []);
+
   return ReactDom.createPortal(
     <div
       className="modal-outer"
       onClick={() => close()}
-      onKeyPress={() => {TabTrap(); close()}}
+      onKeyDown={(e) => TabTrap(e, close)}
       role="button"
       tabIndex="0"
     >
@@ -54,7 +61,7 @@ export default function Modal({ children, close }) {
       {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
       <div
         onClick={(e) => handleInnerClick(e)}
-        onKeyPress={(e) => handleInnerClick(e)}
+        onKeyDown={(e) => TabTrap(e, close)}
         className="modal-innerWrapper"
       >
         {children}

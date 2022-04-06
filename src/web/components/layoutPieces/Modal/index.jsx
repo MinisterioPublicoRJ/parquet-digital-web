23303295b48f3b8e8454d9ae4b617bd8cb9d12a1
re-andrade1987 <renataandrade1987@hotmail.com>
2022-04-06 17:11:27 -0300

    Creating tabtrapping for tab inside modal

--- src/web/components/layoutPieces/Modal/index.jsx
+++ src/web/components/layoutPieces/Modal/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React from 'react';
+import React, {useRef, inputRef} from 'react';
 import PropTypes from 'prop-types';
 import ReactDom from 'react-dom';
 import './styles.css';
@@ -16,14 +16,37 @@ const defaultProps = { children: undefined };
  */
 function handleInnerClick(e) {
   e.stopPropagation();
+
+}
+
+const TabTrap = (e) => {
+  if (e.key !== 'Tab') return
+
+  const focusableModalElements = Modal.current.querySelectorAll(
+    'a[href], button:not([disabled]), textarea, input, select'
+  )
+
+  const firstElement = focusableModalElements[0]
+  const lastElement =
+    focusableModalElements[focusableModalElements.length - 1]
+
+  if (!e.shiftKey && document.activeElement === lastElement) {
+    firstElement.focus();
+    return e.preventDefault();
+  }
+  if (e.shiftKey && document.activeElement === firstElement) {
+    lastElement.focus()
+    e.preventDefault()
+  }
 }
 
 export default function Modal({ children, close }) {
+  
   return ReactDom.createPortal(
     <div
       className="modal-outer"
       onClick={() => close()}
-      onKeyDown={() => close()}
+      onKeyPress={() => {TabTrap(); close()}}
       role="button"
       tabIndex="0"
     >
@@ -31,7 +54,7 @@ export default function Modal({ children, close }) {
       {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
       <div
         onClick={(e) => handleInnerClick(e)}
-        onKeyDown={(e) => handleInnerClick(e)}
+        onKeyPress={(e) => handleInnerClick(e)}
         className="modal-innerWrapper"
       >
         {children}

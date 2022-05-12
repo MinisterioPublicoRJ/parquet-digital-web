3b90471081570922b107c5fe3305ab288a7eae8d
re-andrade1987 <renataandrade1987@hotmail.com>
2022-05-12 18:35:56 -0300

    Changing files and cleaning code

---
+++ src/web/views/dashboard/sections/Glossary/UserManual.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import './styles.css';
+import MANUALPIP from './glossaryPdfs/manual_pip.pdf';
+import { useAppContext } from '../../../../../core/app/App.context';
+
+function UserManual() {
+  const { currentOffice } = useAppContext();
+  const { tipo } = currentOffice;
+  console.log(tipo)
+
+  return (
+    <div className="glossary-wrapper">
+      <div className="glossary-intro">
+        <h2>Manual de uso Parquet Digital</h2>
+        <p>
+          Aqui você poderá conferir todos os termos usados para compor o painel bem como o
+          significados e como cada índice é calculado.
+        </p>
+      </div>
+      <div className="glossary-articles-wrapper">
+        <a 
+         target="_blank"
+         onClick={event => {
+             event.preventDefault();
+             window.open(MANUALPIP);
+           }
+         }
+        >Clique aqui e abra o manual de uso da sua promotoria.
+        </a>        
+      </div>
+    </div>
+  );
+}
+
+export default UserManual;

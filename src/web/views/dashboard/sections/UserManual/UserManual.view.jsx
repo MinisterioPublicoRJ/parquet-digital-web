bfac0debf6a29766044df04da5e67e4ef5618214
majard <mahjard@protonmail.com>
2022-05-18 15:38:08 -0300

    css adjustments

--- src/web/views/dashboard/sections/UserManual/UserManual.view.jsx
+++ src/web/views/dashboard/sections/UserManual/UserManual.view.jsx
@@ -3,7 +3,14 @@ import MANUALPIP from './userManualPdfs/manual_pip.pdf';
 import MANUALPJTC from './userManualPdfs/manual_pjtc.pdf';
 
 import { useAppContext } from '../../../../../core/app/App.context';
-import {glossaryWrapper, glossaryIntroSection, glossaryIntroSubtitle, glossaryIntro, glossaryArticlesWrapper} from './userManual.module.css'
+import {
+  glossaryWrapper,
+  glossaryIntroSection,
+  glossaryIntroSubtitle,
+  glossaryIntro,
+  glossaryArticlesWrapper,
+  promotronIcon
+} from './userManual.module.css';
 import PromotronUserManual from '../../../../assets/svg/promotronUserManual.jsx';
 
 function UserManual() {
@@ -16,37 +23,40 @@ function UserManual() {
         <div className={glossaryIntroSubtitle}>
           <p>Manual de uso</p>
         </div>
-        <h2>Parquet Digital</h2>
-        <div class={glossaryIntroSection}>
-        <p>
-          Este manual visa auxiliar o usuário a potecializar a capacidade da ferramenta em solucionar
-          demandas dentro de sua Promotoria de Justiça
-        </p>
-        <PromotronUserManual />
+        <h2>PARQUET DIGITAL</h2>
+        <div className={glossaryIntroSection}>
+          <p>
+            Este manual visa auxiliar o usuário a potencializar a capacidade da ferramenta em
+            solucionar demandas dentro de sua Promotoria de Justiça
+          </p>
+
+          <div className={glossaryArticlesWrapper}>
+            {tipo === 1 ? (
+              <button
+                type="button"
+                onClick={(event) => {
+                  event.preventDefault();
+                  window.open(MANUALPJTC);
+                }}
+              >
+                Acessar Manual
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={(event) => {
+                  event.preventDefault();
+                  window.open(MANUALPIP);
+                }}
+              >
+                Acessar Manual
+              </button>
+            )}
+          </div>
         </div>
       </div>
-      <div className={glossaryArticlesWrapper}>
-        {tipo === 1 ?(
-          <a 
-          target="_blank"
-          onClick={event => {
-              event.preventDefault();
-              window.open(MANUALPJTC);
-            }
-          }
-         ><p>Acessar Manual</p>
-         </a>    
-        ) : (
-        <a 
-         target="_blank"
-         onClick={event => {
-             event.preventDefault();
-             window.open(MANUALPIP);
-           }
-         }
-        ><p>Acessar Manual</p>
-        </a>   
-        )}     
+      <div className={promotronIcon}>
+        <PromotronUserManual />
       </div>
     </div>
   );

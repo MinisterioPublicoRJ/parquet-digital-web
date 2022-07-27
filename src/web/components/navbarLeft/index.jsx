cac0af2c2e7ad3788efeaea5d93ce0cb1328c0eb
re-andrade1987 <renataandrade1987@hotmail.com>
2022-07-27 16:26:16 -0300

    Created component navbar left

---
+++ src/web/components/navbarLeft/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { mainTitle, mainTitleGlued } from './navBarLeft.module.css';
+
+function NavbarLeft() {
+  return <h1>Navbar</h1>;
+}
+
+export default NavbarLeft;

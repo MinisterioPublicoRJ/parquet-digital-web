3c28add2ecae74e9e83b268792a80a29ff8483f9
re-andrade1987 <renataandrade1987@hotmail.com>
2022-08-01 10:53:10 -0300

    Ajusting design navbar

--- src/web/components/navbarLeft/index.jsx
+++ src/web/components/navbarLeft/index.jsx
@@ -3,32 +3,44 @@ import UserManual  from "../../views/dashboard/sections/UserManual/UserManual.vi
 import  Introduction from "../../views/dashboard/sections/Introduction";
 import { Modal } from '../../components/layoutPieces';
 import { GlossaryBook, IntroScreenInterrogation } from '../../assets';
-import { navBarLeftContent, logoutBtnVisible, logoutBtnVisibleIntro } from './navBarLeft.module.css'
+import { navBarLeftContent, logoutBtnVisible,
+  logoutBtnVisibleIntro,
+  navBarBoxContentTexts } from './navBarLeft.module.css'
 import { useAppContext } from '../../../core/app/App.context';
 
 function NavbarLeft() {
   const [modalType, setModalType] = useState(false);
   const { logout } = useAppContext();
 
+
   return (
   <div className={navBarLeftContent}>
-  <button
-    type="button"
-    onClick={() => setModalType('glossary')}
-  >
-    <GlossaryBook />
-  </button>
+    <div className={navBarBoxContentTexts}>
+      <button
+      type="button"
+      onClick={() => setModalType('glossary')}
+    >
+      <GlossaryBook />
+      </button>
+    <p>Glossário</p>
+    </div>
   {
+
     modalType === 'glossary' &&
     <Modal withExitButton close={setModalType}>
+      <div>
       <UserManual/>
+      </div>
     </Modal>
   }
-  <button type="button" 
+  <div className={navBarBoxContentTexts}>
+    <button type="button" 
     onClick={() => setModalType('introduction')}
-  >
+    >
     <IntroScreenInterrogation />
-  </button>
+    </button>
+    <p>Manual de uso</p>
+  </div>
   {       
     modalType === 'introduction' &&
     <Modal withExitButton close={setModalType}>

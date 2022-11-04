0da2f840fe173e3dd8070239c362f5aac8d9bac1
re-andrade1987 <renataandrade1987@hotmail.com>
2022-11-04 16:33:19 -0300

    inserted endpoint to get data from improbity alert

--- src/web/api/endpoints.js
+++ src/web/api/endpoints.js
@@ -38,6 +38,9 @@ export const TOTAL_ALERTS_LIST = t(`${BASE_URL}/alertas/list/\${orgao}`);
 export const HIRES_ALERTS = t(`${BASE_URL}/alertas/compras/\${orgao}`);
 export const CAVL_ALERTS = t(`${BASE_URL}/alertas/compras_produtos/\${orgao}`);
 
+export const MISCONDUCT_ALERT = t(
+  `${BASE_URL}/alertas/improb_admin/\${orgao}`,
+);
 export const PROCESSING_TIME_DATA = t(`${BASE_URL}/tempo-tramitacao/\${orgao}?version=1.1`);
 
 export const PROCESSES_LIST = t(`${BASE_URL}/lista/processos/\${orgao}?page=\${page}`);

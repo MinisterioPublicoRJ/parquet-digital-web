5880097af6b127c59c4b21fe04b5c520e937c33a
Renata De Almeida Andrade <renata.andrade@mprj.mp.br>
2023-05-31 17:43:10 -0300

    More refactoring and adjust for buttons

--- src/web/views/dashboard/sections/YourDesk/InfoBoxsYourDesk/index.js
+++ src/web/views/dashboard/sections/YourDesk/InfoBoxsYourDesk/index.js
@@ -2,67 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Spinner } from '../../../../../components';
 import { useAppContext } from '../../../../../../core/app/App.context';
-
 import {
   controlButtonOuter,
   controlButtonInner,
-  controlButtonInactive,
-  controlButtonActive,
   controlButtonBigNumber,
-  controlButtonBigNumberActive,
 } from './styles.module.css';
 
 const propTypes = {
-  isActive: PropTypes.bool,
   number: PropTypes.number,
   text: PropTypes.string.isRequired,
-  isButton: PropTypes.bool,
   loading: PropTypes.bool,
-  buttonPressed: PropTypes.func,
   error: PropTypes.bool,
 };
 
 const defaultProps = {
-  isActive: false,
-  isButton: false,
-  buttonPressed: null,
   error: false,
   number: 0,
   loading: false,
 };
 
-function MainButton({ isActive, number, text, isButton, loading, buttonPressed, error }) {
+function MainButton({  number, text, loading, error }) {
   const { currentOffice } = useAppContext();
 
-  let fill;
-  if (isButton) {
-    if (isActive) {
-      fill = (
-        <div
-          className={`${controlButtonInner} ${controlButtonActive}`}
-        >
-          {loading ? (
-            <Spinner size="small" />
-          ) : (
-            <span className={controlButtonBigNumberActive}>{error ? 0 : number}</span>
-          )}
-        </div>
-      );
-    } 
-  } else {
-    fill = (
-      <div className={`${controlButtonInner} ${controlButtonInactive}`}>
-        {loading ? (
-          <Spinner size="small" />
-        ) : (
-          <span className={controlButtonBigNumber}>{error ? 0 : number}</span>
-        )}
-        {text}
-      </div>
-    );
-  }
-
-  return <div className={controlButtonOuter}>{fill}</div>;
+  return <div className={controlButtonOuter}>
+    <div className={`${controlButtonInner}` }
+    >
+      {loading ? (
+        <Spinner size="small" />
+      ) : (
+        <span>{error ? 0 : number}</span>
+      )}
+      {text}
+    </div>
+  </div>;
 }
 
 MainButton.propTypes = propTypes;

84463d1dd4518ecf48689665c237966e59611312
majard <mahjard@protonmail.com>
2023-06-21 16:43:50 -0300

    Gets bars to show properly

--- src/web/views/dashboard/sections/YourDesk/DeskGraph/DeskGraph.view.jsx
+++ src/web/views/dashboard/sections/YourDesk/DeskGraph/DeskGraph.view.jsx
@@ -4,10 +4,7 @@ import PropTypes from 'prop-types';
 import { VictoryBar, VictoryStack } from 'victory';
 import { leftPad } from '../../../../../utils';
 
-import {
-  deskCasesChartOuter,
-  deskCasesChartGraph,
-} from './DeskGraph.module.css';
+import { deskCasesChartOuter, deskCasesChartGraph } from './DeskGraph.module.css';
 
 const propTypes = {
   category: PropTypes.string.isRequired,
@@ -30,42 +27,46 @@ const fillerData = [
   { y: 100, color: 'transparent' },
 ];
 
-function DeskGraph({category, color, data, name }) {
+function DeskGraph({ category, color, data, name }) {
   const [buttonChartData, setButtonChartData] = useState(fillerData);
-  const [totalY, setTotalY] = useState(0);
+  const [colors, setColors] = useState(buttonChartData.map((item) => item.color));
   // anti prop, but it's the only way to force VictoryPie to animate
   useEffect(() => {
     if (buttonChartData === fillerData) {
       setButtonChartData(Object.values(data));
       console.log('data: ', data);
       console.log('values: ', Object.values(data));
-    } 
+    }
   }, [data]);
+
+  useEffect(() => {
+    const c = buttonChartData.map((item) => item.color);
+    console.log('colors c: ', c);
+    setColors(c);
+    console.log('buttonchart data in colors: ', buttonChartData);
+  }, [buttonChartData]);
+
   return (
-    <div
-      style={{ borderTopColor: color }}
-      className={deskCasesChartOuter}
-    >
+    <div style={{ borderTopColor: color }} className={deskCasesChartOuter}>
       <div className={deskCasesChartGraph}>
-        <VictoryStack horizontal>
+        <VictoryStack horizontal colorScale={colors} width={220} height={100} padding={0}>
+          {/*             <VictoryBar horizontal data={buttonChartData} /> */}
+
           {buttonChartData.map((chartData, i) => {
             console.log('chartData, i: ', chartData, i);
-
-            return (<VictoryBar
-              horizontal
-              data={[{y: chartData.y}]}
-              animate={{ duration: 2000 }}
-              style={{
-                data: {
-                  fill: ({ datum }) => datum.color,
-                },
-              }}
-            />
-            )
-
-          }
-          )
-          }
+            return (
+              <VictoryBar
+                horizontal
+                data={[{ y: chartData.y }]}
+                animate={{ duration: 2000 }}
+                barRatio={1}
+                barWidth={50}
+              />
+            );
+          })}
+          {/*   <VictoryBar horizontal data={[{y: 20}]} />
+          <VictoryBar horizontal data={[{y: 50}]} />
+          <VictoryBar horizontal data={[{y: 55}]} /> */}
         </VictoryStack>
       </div>
     </div>

9301eabcab5941ec8af2d4a53be329afe5ba61dd
Renata De Almeida Andrade <renata.andrade@mprj.mp.br>
2023-06-30 19:46:04 -0300

    Ajustes do input de pesquisa

---
+++ src/web/components/layoutPieces/SearchBox/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles.module.css';
+import  SearchIcon from '../../../assets/svg/searchIcon';
+
+const {
+  SearchBoxOuter,
+  SearchBoxInput,
+} = styles;
+
+function SearchBox({ onSearch }) {
+  const searchInput = useRef(null);
+
+
+function handleSearchButtonClick() {
+    onSearch(searchInput.current.value);
+  }
+
+
+  return (
+    <div className={SearchBoxOuter}>
+      <SearchIcon />
+      <input
+          type="text"
+          className={SearchBoxInput}
+          onClick={handleSearchButtonClick}
+          placeholder="Pesquisar na lista"
+          ref={searchInput}
+
+        />
+    </div>
+  );
+}
+
+SearchBox.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
+export default SearchBox;
--- src/web/views/dashboard/sections/YourDesk/OpenCasesList/OpenCasesList.view.jsx
+++ src/web/views/dashboard/sections/YourDesk/OpenCasesList/OpenCasesList.view.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { SearchBox } from 'mapasteca-web';
 import { MAIN_DATA, TABLE_COLUMNS, TAB_MATCHER } from './openCasesConstants';
 import Api from '../../../../../api';
 import { Spinner, CustomTable, Pagination, ProcessDetail } from '../../../../../components';
-import { Modal } from '../../../../../components/layoutPieces';
+import { Modal, SearchBox } from '../../../../../components/layoutPieces';
 import { highlightJSX } from '../../../../../utils';
 
 import {

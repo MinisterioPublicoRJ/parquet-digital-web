ab2c7a8b44484c4642eee4d78fb016b6a8fe1320
Renata De Almeida Andrade <renata.andrade@mprj.mp.br>
2023-08-31 17:45:03 -0300

    Ajustando código

--- src/web/views/dashboard/sections/YourDesk/OpenCasesList/OpenCasesList.view.jsx
+++ src/web/views/dashboard/sections/YourDesk/OpenCasesList/OpenCasesList.view.jsx
@@ -236,7 +236,7 @@ function OpenCasesList({ isLoading, buildRequestParams, chartData }) {
 
   const emptyTab = !chartData[activeTab];
   const LABELS = ['Todas as vistas', 'Até 20 dias', '20 a 30 dias', '+30 dias'];
-  const categories = Object.keys(tabDetails)
+  const categories = Object.keys(chartData)
 
   console.log(categories)
 

4d40dc79a7870e1e2e17665a94c34358ff463201
Renata De Almeida Andrade <renata.andrade@mprj.mp.br>
2023-11-24 17:33:20 -0300

    adding factual news button for general prosecutor's office

--- src/web/views/dashboard/sections/YourDesk/deskConstants.js
+++ src/web/views/dashboard/sections/YourDesk/deskConstants.js
@@ -4,11 +4,15 @@ export const TUTELA_DESK_BUTTONS = ['openCases', 'newDocs', 'closedCasesT'];
 
 export const CRIMINAL_DESK_BUTTONS = ['openCases', 'newDocs', 'closedCasesC'];
 
+export const GENERALIST_DESK_BUTTONS = ['openCases', 'newDocs', 'closedCasesC'];
+
 export const PIP_COLLECTION_BUTTONS = ['pics', 'inquiries', 'aisps'];
 
-export const TUTELA_COLLECTION_BUTTONS = ['courtCases', 'openInvestigations',];
+export const TUTELA_COLLECTION_BUTTONS = ['courtCases', 'openInvestigationsT'];
 
 export const CRIMINAL_COLLECTION_BUTTONS = ['criminalCourtCases'];
+
+export const GENERALIST_COLLECTION_BUTTONS = ['courtCases','openInvestigationsG', 'factualNews'];
 
 export const CONTROL_BUTTONS = ['desk', 'collection',];
 
@@ -22,13 +26,17 @@ export const BUTTON_TEXTS = {
   closedCasesP: `finalizados últimos 30 dias`,
   closedCasesT: `finalizados últimos 30 dias`,
   closedCasesC: `finalizados últimos 30 dias`,
-  courtCases: `Processos\nem juízo`,
-  openInvestigations: `Investigações\nem curso`,
+  courtCases: `Número de processos\nem juízo`,
+  openInvestigationsT: `Investigações\nem curso`,
   criminalCourtCases: `Número de processos\nem juízo`,
   pics: 'Número de PICS',
   inquiries: 'Número de inquéritos',
   aisps: 'Na sua área de atribuição',
   newDocs: 'Documentos novos últimos 30 dias',
+  factualNews: 'Notícias de fato',
+  generalistCourtCases: `Número de processos\nem juízo`,
+  openInvestigationsG: 'Investigações\nem curso'
+
 };
 
 export const BUTTON_COLORS = {
@@ -36,13 +44,15 @@ export const BUTTON_COLORS = {
   closedCasesP: "green",
   closedCasesT: "green",
   closedCasesC: "green",
-  openInvestigations: "gray",
+  openInvestigationsT: "gray",
+  openInvestigationsG: "gray",
   courtCases: "gray",
   criminalCourtCases: "gray",
   pics: "gray",
   inquiries: "gray",
   aisps: "gray",
   newDocs: "blue",
+  factualNews: "gray",
 };
 
 export const BUTTON_DICT = {
@@ -51,10 +61,14 @@ export const BUTTON_DICT = {
   closedCasesP: 'pip_finalizados',
   closedCasesC: 'criminal_finalizados',
   closedCasesT: 'tutela_finalizados',
+  closedCasesG: 'generalista_finalizados',
   pics: 'pip_pics',
   inquiries: 'pip_inqueritos',
   aisps: 'pip_aisp',
-  openInvestigations: 'tutela_investigacoes',
+  openInvestigationsT: 'tutela_investigacoes',
   courtCases: 'tutela_processos',
-  criminalCourtCases: 'criminal_processos'
+  criminalCourtCases: 'criminal_processos',
+  generalistaCourtCases: '',
+  factualNews: '',
+  openInvestigationsG: ''
 };

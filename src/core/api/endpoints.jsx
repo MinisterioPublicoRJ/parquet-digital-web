2f96c50c4f70bcca13e6b01c02278cb09faeda54
Renata De Almeida Andrade <renata.andrade@mprj.mp.br>
2023-12-06 16:13:13 -0300

    Adicioando vite e retirando create react app

--- src/core/api/endpoints.jsx
+++ src/core/api/endpoints.jsx
@@ -1,6 +1,7 @@
 import t from 'lodash.template';
 
-export const BASE_URL = `${process.env.REACT_APP_BASE_URL}/dominio`;
+// export const BASE_URL = `${process.env.REACT_APP_BASE_URL}/dominio`;
+export const BASE_URL = `${import.meta.env.REACT_APP_BASE_URL}/dominio`;
 
 export const SCA_LOGIN = `/token/login-promotron/`;
 
